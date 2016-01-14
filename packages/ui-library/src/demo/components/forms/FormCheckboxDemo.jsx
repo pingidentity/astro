00f764cfd743469f9771275c9a15e3c7c6ba9e90
Eric Lezotte <elezotte@pingidentity.com>
2016-01-14 10:49:43 -0700

    ID-3630 remove uppercase styling for all radio labels add new inline css style for when checkbox options are listed horizontally update checkbox demo
    Change-Id: I83c748e251aab24573d9c1b4f2e2b8a5b943d514

--- packages/ui-library/src/demo/components/forms/FormCheckboxDemo.jsx
+++ packages/ui-library/src/demo/components/forms/FormCheckboxDemo.jsx
@@ -7,33 +7,18 @@ var FormCheckbox = require("./../../../components/forms/FormCheckbox.jsx");
 
 var FormCheckboxDemo = React.createClass({
     getInitialState: function () {
-        return{
-            checkboxChecked1: false,
-            checkboxChecked2: true,
-            checkboxChecked3: true,
-            checkboxChecked4: false
-        };
-    },
-    _onChange1: function (event) {
+        var initialState = {};
 
-        this.setState({
-            checkboxChecked1: event.target.checked
-        });
+        for (var i=1; i<=10; i+=1) {
+            initialState["checkboxChecked" + i] = Math.random() < 0.5;
+        }
+
+        return initialState;
     },
-    _onChange2: function (event) {
-        this.setState({
-            checkboxChecked2: event.target.checked
-        });
-    },
-    _onChange3: function (event) {
-        this.setState({
-            checkboxChecked3: event.target.checked
-        });
-    },
-    _onChange4: function (event) {
-        this.setState({
-            checkboxChecked4: event.target.checked
-        });
+    _onChange: function (i, event) {
+        var newState = {};
+        newState["checkboxChecked" + i] = event.target.checked;
+        this.setState(newState);
     },
     render: function () {
         return (
@@ -42,35 +27,80 @@ var FormCheckboxDemo = React.createClass({
                     <FormCheckbox
                         label = {"Regular Checkbox; checked: " + this.state.checkboxChecked1}
                         value = ""
-                        onChange = {this._onChange1}
+                        onChange = {this._onChange.bind(this, 1)}
                         checked = {this.state.checkboxChecked1}
-                        />
+                    />
                 </div>
                 <div className="input-row">
                     <FormCheckbox
                         label = {"Default Checked; checked: " + this.state.checkboxChecked2}
                         value = ""
-                        onChange = {this._onChange2}
+                        onChange = {this._onChange.bind(this, 2)}
                         checked = {this.state.checkboxChecked2}
-                        />
+                    />
                 </div>
                 <div className="input-row">
                     <FormCheckbox
                         label = {"Disabled; checked: " + this.state.checkboxChecked3}
                         value = ""
-                        onChange = {this._onChange3}
+                        onChange = {this._onChange.bind(this, 3)}
                         checked = {this.state.checkboxChecked3}
                         disabled = {true}
-                        />
+                    />
                 </div>
                 <div className="input-row">
                     <FormCheckbox
-                        label = {"With added Help Text; checked: " + this.state.checkboxChecked4
-                        }
+                        label = {"With added Help Text; checked: " + this.state.checkboxChecked4}
                         value = ""
-                        onChange = {this._onChange4}
+                        onChange = {this._onChange.bind(this, 4)}
                         checked = {this.state.checkboxChecked4}
                         labelHelpText = "Check this box!"
+                    />
+                </div>
+                <div className="input-row">
+                    <label className="detached">
+                        Example of Stacked Options
+                    </label>
+                    <FormCheckbox
+                        label="Stacked Checkbox"
+                        onChange={this._onChange.bind(this, 5)}
+                        checked={this.state.checkboxChecked5}
+                        className="stacked"
+                    />
+                    <FormCheckbox
+                        label="Stacked Checkbox"
+                        onChange={this._onChange.bind(this, 6)}
+                        checked={this.state.checkboxChecked6}
+                        className="stacked"
+                    />
+                    <FormCheckbox
+                        label="Stacked Checkbox"
+                        onChange={this._onChange.bind(this, 7)}
+                        checked={this.state.checkboxChecked7}
+                        className="stacked"
+                    />
+                </div>
+                <div className="input-row">
+                    <label className="detached">
+                        Example of Inline Options
+                    </label>
+                    <FormCheckbox
+                        label="Inline Checkbox"
+                        onChange={this._onChange.bind(this, 8)}
+                        checked={this.state.checkboxChecked8}
+                        className="inline"
+                    />
+                    <FormCheckbox
+                        label="Inline Checkbox"
+                        onChange={this._onChange.bind(this, 9)}
+                        checked={this.state.checkboxChecked9}
+                        className="inline"
+                    />
+                    <FormCheckbox
+                        label="Inline Checkbox"
+                        onChange={this._onChange.bind(this, 10)}
+                        checked={this.state.checkboxChecked10}
+                        className="inline"
                     />
                 </div>
             </div>
