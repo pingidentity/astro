3bbd7ddb8d839648f19985c1deb3c4241fcb9e41
Eric Lezotte <elezotte@pingidentity.com>
2016-02-29 15:24:42 -0700

    ID-4416 ID-4425 fix arrow behavior for existing collapsable section add new collapsable section css (to be used in new component with new markup) add trimmed version of drop arrow (existing had too much padding/extra space)
    Change-Id: Ieaf57bd0c3d769cd149ab62b59b9369cf4d87d37

--- packages/ui-library/src/demo/components/general/CollapsibleSectionDemo.jsx
+++ packages/ui-library/src/demo/components/general/CollapsibleSectionDemo.jsx
@@ -5,28 +5,40 @@ var CollapsibleSectionDemo = React.createClass({
 
     getInitialState: function () {
         return {
-            open: false
+            open: false,
+            openNew: false
         };
     },
 
-    _toggle: function (isOpen) {
+    _toggle: function () {
         this.setState({
-            open: !isOpen
+            open: !this.state.open
         });
     },
 
     render: function () {
         return (
-            <Section className="condition-title" controlled={false} onToggle={this._toggle} expanded={this.state.open}>
+            <Section
+                className="condition-title"
+                controlled={false}
+                onToggle={this._toggle}
+                expanded={this.state.open}>
+
                 <span title={true} className="icon-dropdown-arrow show-condition"></span>
                 Collapsible Title
 
                 <div className="condition">
                     <div className="condition-fields">
-                        <p>This is a text field</p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas lectus vulputate
+                            diam porta rhoncus. Aenean sollicitudin nunc dui, eget cursus nisi interdum vitae. Donec
+                            nec justo quis velit ullamcorper dictum vel sed quam. Etiam purus libero, porttitor
+                            vitae risus ac, venenatis luctus lacus.
+                        </p>
                     </div>
                 </div>
             </Section>
+
         );
     }
 });
