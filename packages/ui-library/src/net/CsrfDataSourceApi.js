31bad5551b6bd0c135bbeb8f2abff7a168e789c0
acojocaru <acojocaru@pingidentity.com>
2016-03-16 15:49:57 -0700

    ID-3413 -- add DataSourceApi wrapper for CSRF protection support
    Change-Id: Idf08e530d1a6b5e8627984229d8b3f7d1383b8e3

---
+++ packages/ui-library/src/net/CsrfDataSourceApi.js
@@ -0,0 +1,197 @@
+"use strict";
+
+/**
+ * @module net/CsrfDataSourceApi
+ *
+ * @desc A wrapper on {@link net/DataSourceApi} with support for CSRF protection.
+ *
+ * Caching is set by default, unless using getNoCache() method.
+ *
+ * Parsing the response: If the server returns responses in JSON format,
+ * then it will get automatically parsed in the "response.data"
+ * so if the JSON has a property like: "phone" then all you have to do is to read its value
+ * from "response.data.phone"
+ *
+ * @example
+ * var api = require("/ui-library/src/net/CsrfDataSourceApi")({
+ *     rootPath: "/web-portal/",
+ *     csrfHeaderName: "X-CSRF-TOKEN",
+ *     csrfCookieName: "web-portal-csrf"
+ * });
+ *
+ * // GET Request
+ * api.get("myendpoint/action", { queryParam: "q1" }, function (response) {
+ *     if (!response.error) {
+ *         console.log("Received " + response.status + " with data: " + response.data);
+ *     } else {
+ *         console.log("Received error: "  + response.error);
+ *     }
+ *
+ * });
+ *
+ * // POST Request
+ * api.post("myendpoint/action", { postParam: "p1" }, { queryParam: "q1" }, function (response) {
+ *     if (!response.error) {
+ *         console.log("Received " + response.status + " with data: " + response.data);
+ *     } else {
+ *         console.log("Received error: "  + response.error);
+ *     }
+ * });
+ */
+
+var DataSourceApi = require("./DataSourceApi"),
+    _ = require("underscore");
+
+
+/**
+ * @desc Read the value of the cookie with the given name.
+ * @private
+ * @param {string} name the cookie name
+ * @return {?string} the cookie value or null
+ */
+var readCookie = function (name) {
+    var nameEq = name + "=";
+    var targetCookie = _.find(
+        document.cookie.split(";"),
+        function (cookie) {
+            return cookie.replace(/^\s\s*/, "").indexOf(nameEq) === 0;
+        }
+    );
+    return targetCookie ? targetCookie.trim().slice(nameEq.length) : null;
+};
+
+/**
+ * @desc Write a cookie with the given name, value and max age.
+ * @private
+ * @param {string} name the cookie name
+ * @param {string} value the cookie value
+ * @param {number} maxAge the max age of the cookie
+ */
+var writeCookie = function (name, value, maxAge) {
+    var cookie = name + "=" + value + "; max-age=" + maxAge + "; path=/";
+    document.cookie = cookie;
+};
+
+/**
+ * @desc Read the CSRF protection token from the heads on the given response object,
+ *     and, if found, store it in a cookie to be used on a subsequent request to the server.
+ * @private
+ * @param {object} response the super agent response
+ * @param {string} headerName the name of the header containing the CSRF protection token
+ *     The method is a no-op if the headerName is falsy.
+ * @param {string} cookieName the name of the cookie to store the CSRF protection token
+ */
+var parseCsrfHeader = function (response, headerName, cookieName) {
+    if (headerName) {
+        var csrfToken = response.headers[headerName.toLowerCase()];
+        if (csrfToken) {
+            writeCookie(cookieName, csrfToken, 3600);
+        }
+    }
+};
+
+/**
+ * @desc Build a headers object (including the CSRF protection token if available)
+ *     to be used on a POST/PUT/DELETE/PATCH request.
+ * @private
+ * @param {string} headerName the name of the header to set with the CSRF protection token as value.
+ * @param {string} cookieName the name of the cookie which contains the CSRF protection token
+ * @return {object} the headers object to be used on the super agent request. If the header name
+ *     is falsy, an empty object is returned.
+ */
+var buildCsrfHeader = function (headerName, cookieName) {
+    var headers = {};
+
+    if (headerName) {
+        var csrfToken = readCookie(cookieName);
+        if (csrfToken !== null) {
+            headers[headerName] = csrfToken;
+        }
+    }
+
+    return headers;
+};
+
+/**
+ * @typedef GetRequest
+ * @type {function}
+ * @param {string} endpoint the endpoint to send the request to
+ * @param {object} params the GET request parameters
+ * @param {function} callback a function to be called back with the response
+ */
+
+/**
+ * @typedef CsrfDataSourceApiResult
+ * @type {object}
+ * @property {GetRequest} get make a GET request; it uses the local cached data if available and valid
+ * @property {GetRequest} getNoCache make a GET request; it bypasses the local cache
+ * @property {function} post make a POST request
+ * @property {function} put make a PUT request
+ * @property {function} doDelete make a DELETE request
+ */
+
+/**
+ * @desc Build a DataSourceApi with CSRF protection enabled, configured with the supplied config object.
+ * @param {object} config the DataSourceApi config
+ * @param {string} config.rootPath it is used to to prefix each endpoint URL
+ * @param {string} [config.csrfHeaderName] the name of the CSRF protection header sent by
+ *    the server to the client and back
+ * @param {string} [config.csrfCookiename="application-csrf"] the cookie name to store
+ *    the CSRF protection token received from the server
+ * @return {CsrfDataSourceApiResult} a DataSourceApi configured for the root path
+ *    and CSRF protection as defined by the provided config
+ */
+var CsrfDataSourceApi = function (config) {
+
+    var rootPath = config && config.rootPath || "";
+
+    var csrfHeaderName = config && config.csrfHeaderName;
+    var csrfCookieName = config && config.csrfCookieName || "application-csrf";
+
+    return {
+        get: function (endpoint, params, callback) {
+            DataSourceApi.get(
+                    rootPath + endpoint,
+                    params,
+                    function (response) {
+                        parseCsrfHeader(response, csrfHeaderName, csrfCookieName);
+                        callback(response);
+                    });
+        },
+        getNoCache: function (endpoint, params, callback) {
+            DataSourceApi.getNoCache(
+                    rootPath + endpoint,
+                    params,
+                    function (response) {
+                        parseCsrfHeader(response, csrfHeaderName, csrfCookieName);
+                        callback(response);
+                    });
+        },
+        post: function (endpoint, data, params, callback, files) {
+            DataSourceApi.post(
+                    rootPath + endpoint,
+                    data,
+                    params,
+                    callback,
+                    files,
+                    buildCsrfHeader(csrfHeaderName, csrfCookieName));
+        },
+        put: function (endpoint, data, params, callback) {
+            DataSourceApi.put(
+                    rootPath + endpoint,
+                    data,
+                    params,
+                    callback,
+                    buildCsrfHeader(csrfHeaderName, csrfCookieName));
+        },
+        doDelete: function (endpoint, params, callback) {
+            DataSourceApi.doDelete(
+                    rootPath + endpoint,
+                    params,
+                    callback,
+                    buildCsrfHeader(csrfHeaderName, csrfCookieName));
+        }
+    };
+};
+
+module.exports = CsrfDataSourceApi;
---
+++ packages/ui-library/src/net/tests/CsrfDataSourceApiTest.js
@@ -0,0 +1,252 @@
+window.__DEV__ = true;
+
+jest.dontMock("../CsrfDataSourceApi");
+
+describe("CsrfDataSourceApi", function () {
+
+    var api;
+    var callback;
+
+    var apiExport;
+
+    beforeEach(function () {
+        // configure the DataSourceApi mock instance, to allow controlling the DataSourceApi mock
+        apiExport = {
+            get: jest.genMockFunction(),
+            getNoCache: jest.genMockFunction(),
+            post: jest.genMockFunction(),
+            put: jest.genMockFunction(),
+            doDelete: jest.genMockFunction()
+        };
+        jest.setMock("../DataSourceApi", apiExport);
+
+        api = require("../CsrfDataSourceApi")({
+            rootPath: "/web-portal/",
+            csrfHeaderName: "X-CSRF-TOKEN",
+            csrfCookieName: "web-portal-csrf"
+        });
+        callback = jest.genMockFunction();
+
+        // delete the cookies with the 2 main names used in this test class;
+        // if a test uses a different name, it has to be deleted explicitly
+        document.cookie = "web-portal-csrf=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+        document.cookie = "application-csrf=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    });
+
+    it("GET is called", function () {
+        var params = { param1: "value 1" };
+        api.get("endpoint", params, callback);
+
+        expect(apiExport.get.mock.calls.length).toEqual(1);
+        expect(apiExport.get.mock.calls[0][0]).toEqual("/web-portal/endpoint");
+        expect(apiExport.get.mock.calls[0][1]).toBe(params);
+    });
+
+    it("GET no cache is called", function () {
+        var params = { param1: "value 1" };
+        api.getNoCache("endpoint", params, callback);
+
+        expect(apiExport.getNoCache.mock.calls.length).toEqual(1);
+        expect(apiExport.getNoCache.mock.calls[0][0]).toEqual("/web-portal/endpoint");
+        expect(apiExport.getNoCache.mock.calls[0][1]).toBe(params);
+    });
+
+    it("POST is called with CSRF token", function () {
+        document.cookie = "web-portal-csrf=token; max-age=3600; path=/";
+
+        var params = { param1: "value 1" };
+        var data = { "field name 1": "field value" };
+        var files = { filename: "file content" };
+        api.post("endpoint", data, params, callback, files);
+
+        expect(apiExport.post.mock.calls.length).toEqual(1);
+        expect(apiExport.post.mock.calls[0][0]).toEqual("/web-portal/endpoint");
+        expect(apiExport.post.mock.calls[0][1]).toBe(data);
+        expect(apiExport.post.mock.calls[0][2]).toBe(params);
+        expect(apiExport.post.mock.calls[0][4]).toBe(files);
+
+        // check the CSRF token which was retrieved from the document cookie
+        expect(apiExport.post.mock.calls[0][5]["X-CSRF-TOKEN"]).toEqual("token");
+    });
+
+    it("PUT is called with CSRF token", function () {
+        document.cookie = "web-portal-csrf=token; max-age=3600; path=/";
+
+        var params = { param1: "value 1" };
+        var data = { "field name 1": "field value" };
+        api.put("endpoint", data, params, callback);
+
+        expect(apiExport.put.mock.calls.length).toEqual(1);
+        expect(apiExport.put.mock.calls[0][0]).toEqual("/web-portal/endpoint");
+        expect(apiExport.put.mock.calls[0][1]).toBe(data);
+        expect(apiExport.put.mock.calls[0][2]).toBe(params);
+
+        // check the CSRF token which was retrieved from the document cookie
+        expect(apiExport.put.mock.calls[0][4]["X-CSRF-TOKEN"]).toEqual("token");
+    });
+
+    it("DELETE is called with CSRF token", function () {
+        document.cookie = "web-portal-csrf=token; max-age=3600; path=/";
+
+        var params = { param1: "value 1" };
+        api.doDelete("endpoint", params, callback);
+
+        expect(apiExport.doDelete.mock.calls.length).toEqual(1);
+        expect(apiExport.doDelete.mock.calls[0][0]).toEqual("/web-portal/endpoint");
+        expect(apiExport.doDelete.mock.calls[0][1]).toBe(params);
+
+        // check the CSRF token which was retrieved from the document cookie
+        expect(apiExport.doDelete.mock.calls[0][3]["X-CSRF-TOKEN"]).toEqual("token");
+    });
+
+    it("set CSRF cookie on GET response", function () {
+        // mock the get function in the core DataSourceApi, so that it calls the callback function
+        apiExport.get = jest.genMockFunction().mockImplementation(function (endpoint, params, callbackFn) {
+            callbackFn({
+                headers: {
+                    "x-csrf-token": "VALUE"
+                }
+            });
+        });
+
+        // verify that there's no cookie set already
+        expect(document.cookie).toEqual("");
+
+        api.get("endpoint", {}, callback);
+
+        // the cookie must have been set by the get method call
+        expect(document.cookie).toEqual("web-portal-csrf=VALUE");
+
+        expect(callback.mock.calls.length).toEqual(1);
+    });
+
+    it("set CSRF cookie on GET no cache response", function () {
+        // mock the get function in the core DataSourceApi, so that it calls the callback function
+        apiExport.getNoCache = jest.genMockFunction().mockImplementation(function (endpoint, params, callbackFn) {
+            callbackFn({
+                headers: {
+                    "x-csrf-token": "VALUE"
+                }
+            });
+        });
+
+        // verify that there's no cookie set already
+        expect(document.cookie).toEqual("");
+
+        api.getNoCache("endpoint", {}, callback);
+
+        // the cookie must have been set by the get method call
+        expect(document.cookie).toEqual("web-portal-csrf=VALUE");
+
+        expect(callback.mock.calls.length).toEqual(1);
+    });
+
+    it("doesn't set cookie if there's no such header in GET response", function () {
+        // mock the get function in the core DataSourceApi, so that it calls the callback function
+        apiExport.get = jest.genMockFunction().mockImplementation(function (endpoint, params, callbackFn) {
+            callbackFn({
+                headers: {}
+            });
+        });
+
+        // verify that there's no cookie set already
+        expect(document.cookie).toEqual("");
+
+        api.get("endpoint", {}, callback);
+
+        // the cookie must have not been set by the get method call, this is the default value
+        expect(document.cookie).toEqual("");
+    });
+
+    it("doesn't send CSRF token if no such cookie exists", function () {
+        // verify that there's no cookie set already
+        expect(document.cookie).toEqual("");
+
+        var params = { param1: "value 1" };
+        api.doDelete("endpoint", params, callback);
+
+        expect(apiExport.doDelete.mock.calls.length).toEqual(1);
+        expect(apiExport.doDelete.mock.calls[0][0]).toEqual("/web-portal/endpoint");
+        expect(apiExport.doDelete.mock.calls[0][1]).toBe(params);
+
+        // check the CSRF token sent is the defeault one
+        expect(apiExport.doDelete.mock.calls[0][3]).toEqual({});
+    });
+
+    it("use default cookie name if none provided", function () {
+        api = require("../CsrfDataSourceApi")({
+            rootPath: "/web-portal/",
+            csrfHeaderName: "X-CSRF-TOKEN"
+        });
+
+        // mock the get function in the core DataSourceApi, so that it calls the callback function
+        apiExport.get = jest.genMockFunction().mockImplementation(function (endpoint, params, callbackFn) {
+            callbackFn({
+                headers: {
+                    "x-csrf-token": "VALUE"
+                }
+            });
+        });
+
+        // verify that there's no cookie set already
+        expect(document.cookie).toEqual("");
+
+        // make a GET request
+        api.get("endpoint", {}, callback);
+
+        // the cookie must have been set by the get method call
+        expect(document.cookie).toEqual("application-csrf=VALUE");
+
+        // make a PUT request
+        api.put("endpoint", {}, {}, callback);
+
+        // one such request should've been sent out
+        expect(apiExport.put.mock.calls.length).toEqual(1);
+
+        // check the CSRF token which was retrieved from the document cookie
+        expect(apiExport.put.mock.calls[0][4]["X-CSRF-TOKEN"]).toEqual("VALUE");
+    });
+
+    it("set '' as default root path if none provided", function () {
+        api = require("../CsrfDataSourceApi")();
+
+        api.get("endpoint", {}, callback);
+
+        expect(apiExport.get.mock.calls.length).toEqual(1);
+        expect(apiExport.get.mock.calls[0][0]).toEqual("endpoint");
+    });
+
+    it("does not parse/store token if the header name is not specified", function () {
+        api = require("../CsrfDataSourceApi")({
+            rootPath: "/web-portal/"
+        });
+
+        // mock the get function in the core DataSourceApi, so that it calls the callback function
+        apiExport.get = jest.genMockFunction().mockImplementation(function (endpoint, params, callbackFn) {
+            callbackFn({
+                headers: {
+                    "x-csrf-token": "VALUE"
+                }
+            });
+        });
+
+        // verify that there's no cookie set already
+        expect(document.cookie).toEqual("");
+
+        // make a GET request
+        api.get("endpoint", {}, callback);
+
+        // no cookie must have been set by the get method call
+        expect(document.cookie).toEqual("");
+
+        // make a PUT request
+        api.put("endpoint", {}, {}, callback);
+
+        // one such request should've been sent out
+        expect(apiExport.put.mock.calls.length).toEqual(1);
+
+        // check that the headers on the PUT request are empty
+        expect(apiExport.put.mock.calls[0][4]).toEqual({});
+    });
+
+});
