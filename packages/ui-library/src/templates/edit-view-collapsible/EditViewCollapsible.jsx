2f6717004dadb7c239b9c958c4d238fa71841911
Jared Scott <jscott@pingidentity.com>
2016-07-14 12:17:35 -0700

    UIP-549 UIP-628 v2 standards for EditViewCollapsible
    Change-Id: I258a5c56561d6fb7ebad087ee747f66b735b5aa8

--- packages/ui-library/src/templates/edit-view-collapsible/EditViewCollapsible.jsx
+++ packages/ui-library/src/templates/edit-view-collapsible/EditViewCollapsible.jsx
@@ -2,20 +2,42 @@ var React = require("react"),
     FormCheckbox = require("../../components/forms/FormCheckbox.jsx"),
     FormTextField = require("../../components/forms//form-text-field").v2,
     FormRadioGroup = require("../../components/forms/FormRadioGroup.jsx"),
-    FormSelectField = require("../../components/forms/FormSelectField.jsx"),
+    FormSelectField = require("../../components/forms/form-select-field").v2,
     Layout = require("../../components/general/ColumnLayout.jsx"),
     Section = require("../../components/general/Section.jsx"),
-    Toggle = require("../../components/forms/Toggle.jsx");
+    Toggle = require("../../components/forms/form-toggle").v2;
+
+/**
+ * @callback EditViewCollapsible~onInputChange
+ * @param {string} name
+ *     Name of section
+ * @param {string} value
+ *      Value of section
+ */
+
+/**
+ * @callback EditViewCollapsible~onSave
+ */
+
+/**
+ * @callback EditViewCollapsible~onSectionToggle
+ * @param {number} index
+ *     Index of section to expand or collapse
+ * @param {boolean} expand
+ *      Whether to expand or collapse section
+ */
 
 /**
  * @class EditViewCollapsible
  * @desc This is a template to demonstrate how to build a form page with collapsible sections. Use
  *     it as a starting poing for an edit page of this type.
  *
- * @param {function} onInputChange - A callback executed when an input value changes
- * @param {function} onSave - A callback executed when the save button is clicked
- * @param {function} onSectionToggle - A callback to be executed when a collapsible section is
- *     clicked/toggled.
+ * @param {EditViewCollapsible~onInputChange} onInputChange
+ *          Callback to be triggered when an input value changes
+ * @param {EditViewCollapsible~onSave} onSave
+ *          Callback to be triggered when the save button is clicked
+ * @param {EditViewCollapsible~onSectionToggle} onSectionToggle
+ *          Callback to be triggered when a collapsible section is clicked/toggled.
  */
 module.exports = React.createClass({
 
@@ -107,7 +129,6 @@ module.exports = React.createClass({
                                 <div className="input-row">
                                     <FormTextField
                                         className="input-width-medium"
-                                        onChange={this._handleInputChange}
                                         data-id="address2"
                                         value={this.props.inputs.address2}
                                         onChange={this._handleInputChange} />
@@ -178,9 +199,8 @@ module.exports = React.createClass({
                             <FormRadioGroup
                                 label="User Group"
                                 groupName="user-group"
-                                groupName="userGroup"
                                 selected={this.props.inputs.userGroup}
-                                onChange={this._handleRadioInputChange.bind(this, "userGroup")}
+                                onValueChange={this._handleRadioInputChange.bind(this, "userGroup")}
                                 items={[
                                     { id: 1, name: "Group 1" },
                                     { id: 2, name: "Group 2" },
@@ -191,9 +211,9 @@ module.exports = React.createClass({
                             <FormCheckbox
                                 label="Activate User"
                                 className="input-width-medium"
-                                id="userActive"
+                                data-id="userActive"
                                 checked={this.props.inputs.userActive}
-                                onChange={this._handleInputChange} />
+                                onValueChange={this._handleInputChange} />
                         </div>
                     </Section>
                 </div>
