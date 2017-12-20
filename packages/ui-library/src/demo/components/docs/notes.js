03334449ce8c9dd7f0c1a1de5043f5a4b6213fa0
Eric Lezotte <elezotte@pingidentity.com>
2017-12-20 16:45:57 -0700

    add v3.1.0 release notes.
    Change-Id: Ib292a506e5b6e7aee6c66f79d6e90591d8d27d3c

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.1.0",
+        file: require("./release-notes/v3-1-0.md")
+    },
+    {
         title: "v3.0.0",
         file: require("./release-notes/v3-0-0.md")
     },
