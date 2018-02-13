1f6504bdf271660a43b2ed3ac8ef7edb1446a1fe
Eric Lezotte <elezotte@pingidentity.com>
2018-02-13 15:50:39 -0700

    create v3.2.0 release notes
    Change-Id: Iafed1f96302b371d68953dfaa80eba4d35d13b19

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.2.0",
+        file: require("./release-notes/v3-2-0.md")
+    },
+    {
         title: "v3.1.0",
         file: require("./release-notes/v3-1-0.md")
     },
