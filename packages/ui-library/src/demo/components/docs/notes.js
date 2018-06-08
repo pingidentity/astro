5b6af2c0c5042f8ab38a45096e174c1cddbb95b9
Eric Lezotte <elezotte@gmail.com>
2018-06-08 13:25:39 -0600

    add 3.8.0 relase notes
    Change-Id: I904d82d20b2b40a316c2761421ca9dcde1f43e2a

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.8.0",
+        file: require("./release-notes/v3-8-0.md")
+    },
+    {
         title: "v3.7.0",
         file: require("./release-notes/v3-7-0.md")
     },
