84c35726cd3d0e08f48c8251c23910968c1f9f5b
Eric L <elezotte@gmail.com>
2018-07-23 14:51:22 -0600

    v3.13.0 release notes
    Change-Id: Ie7685a8b644d6ce7381a35102b77904d2c90889e

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.13.0",
+        file: require("./release-notes/v3-13-0.md")
+    },
+    {
         title: "v3.12.0",
         file: require("./release-notes/v3-12-0.md")
     },
