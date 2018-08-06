f074d4dd1ffbb0052e4e8f2eee6c44f6f95beb16
Jeff Fal <jfal@pingidentity.com>
2018-08-06 11:45:15 -0600

    UIP-1690 New datetime helper functions
    Change-Id: Ic0a89cc22eb4d06bccb1072c347d8487faff4d3d

--- packages/ui-library/src/util/DateUtils.js
+++ packages/ui-library/src/util/DateUtils.js
@@ -1,9 +1,11 @@
 /*eslint-disable valid-jsdoc*/
 
 "use strict";
-var moment = require("moment");
+const moment = require("moment");
+const dateFormat = "YYYY-MM-DD";
+const timeFormat = "hh:MM:SSa";
 
-var DateUtils = {
+const DateUtils = {
     /**
      * @alias module:util/Utils.formatDate
      * @desc Format the given timestamp in milliseconds as a YYYY-MM-DD date.
@@ -14,8 +16,34 @@ var DateUtils = {
      *     Formatted date
      */
     formatDate: function (timestamp) {
-        return moment.utc(timestamp).format("YYYY-MM-DD");
-    }
+        return moment.utc(timestamp).format(dateFormat);
+    },
+
+    /**
+     * @alias module:util/Utils.formatTime
+     * @desc Format the given timestamp in milliseconds as a YYYY-MM-DD hh:MM:SSa date.
+     *
+     * @param {number} timestamp
+     *     The timestamp (UNIX time * 1000)
+     * @returns {string}
+     *     Formatted date+time
+     */
+    formatDateTime: function (timestamp) {
+        return moment.utc(timestamp).format(`${dateFormat} ${timeFormat}`);
+    },
+
+    /**
+     * @alias module:util/Utils.formatTime
+     * @desc Format the given timestamp in milliseconds as a hh:MM:SSa time.
+     *
+     * @param {number} timestamp
+     *     The timestamp (UNIX time * 1000)
+     * @returns {string}
+     *     Formatted time
+     */
+    formatTime: function (timestamp) {
+        return moment.utc(timestamp).format(timeFormat);
+    },
 };
 
 module.exports = DateUtils;
--- packages/ui-library/src/util/tests/DateUtilsTests.js
+++ packages/ui-library/src/util/tests/DateUtilsTests.js
@@ -12,4 +12,20 @@ describe("DateUtils", function () {
             expect(DateUtils.formatDate(2525465544000)).toBe("2050-01-10");
         });
     });
+
+    describe("formatDateTime", function () {
+        it("formats date to YYYY-MM-DD hh:MM:SSa", function () {
+            expect(DateUtils.formatDateTime(1444860746000)).toBe("2015-10-14 10:10:00pm");
+            expect(DateUtils.formatDateTime(0)).toBe("1970-01-01 12:01:00am");
+            expect(DateUtils.formatDateTime(2525465544000)).toBe("2050-01-10 10:01:00pm");
+        });
+    });
+
+    describe("formatTime", function () {
+        it("formats date to hh:MM:SSa", function () {
+            expect(DateUtils.formatTime(1444860746000)).toBe("10:10:00pm");
+            expect(DateUtils.formatTime(0)).toBe("12:01:00am");
+            expect(DateUtils.formatTime(2525465544000)).toBe("10:01:00pm");
+        });
+    });
 });
