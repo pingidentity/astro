0dc3cf97193dcfb7fe5c169938327a5d4595de08
Gwyneth Rose <gwynethrose@pingidentity.com>
2018-08-15 11:16:01 -0600

    3.15.0 release notes
    Change-Id: Ib69d2b59bf93777b204bfce044e6769febf31430

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.15.0",
+        file: require("./release-notes/v3-15-0.md")
+    },
+    {
         title: "v3.14.0",
         file: require("./release-notes/v3-14-0.md")
     },
