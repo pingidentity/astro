102ae07a3b5e9732eda789c9117d2f48695ce20c
Eric L <elezotte@gmail.com>
2018-10-12 16:52:29 -0600

    3.20.0 release notes
    Change-Id: Idd975ec74bc991fab4b80ae9a8813820a417b5ec

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.20.0",
+        file: require("./release-notes/v3-20-0.md")
+    },
+    {
         title: "v3.19.0",
         file: require("./release-notes/v3-19-0.md")
     },
