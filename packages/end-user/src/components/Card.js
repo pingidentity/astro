cf296704e0db8ad0f5cc54f336f45f4702906292
Eric Lezotte <elezotte@pingidentity.com>
2018-11-30 18:31:01 +0000

    Merge branch 'UIP-1944' into 'master'
    UIP-1944 added invalid sign-on error template and made card wider for this specific one
    
    Closes UIP-1944
    
    See merge request ux/end-user!1

--- packages/end-user/src/components/Card.js
+++ packages/end-user/src/components/Card.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const Card = ({ children }) => <div className="card">{children}</div>;
+const Card = ({ children, width }) => {
+    const classNames = classnames('card', {
+        'card--wide': width === 'large',
+    });
+
+    return (
+        <div className={classNames}>{children}</div>
+    );
+};
+
+Card.propTypes = {
+    width: PropTypes.oneOf(['large']),
+};
 
 export default Card;
