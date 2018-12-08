4f4a3814b1eaddb24f4833a788f304d2f3f2c38f
Eric Lezotte <elezotte@pingidentity.com>
2018-12-07 17:18:53 -0700

    3.27.0 release notes
    Change-Id: I6a35249e5380aa95ab7a32d709e15f70dd1e462c

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.27.0",
+        file: require("./release-notes/v3-27-0.md")
+    },
+    {
         title: "v3.26.0",
         file: require("./release-notes/v3-26-0.md")
     },
