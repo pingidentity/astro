3ac0631592bde96b5a910f0e8bfa7ebaf02de362
Eric Lezotte <elezotte@pingidentity.com>
2019-03-22 16:05:10 -0600

    3.36.0 release notes
    Change-Id: If2b1b1fa3a54deab1d0a76b00c7359824d514991

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.36.0",
+        file: require("./release-notes/v3-36-0.md")
+    },
+    {
         title: "v3.35.0",
         file: require("./release-notes/v3-35-0.md")
     },
