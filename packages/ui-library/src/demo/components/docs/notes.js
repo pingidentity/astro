fa94b1aeca4dc0344ab98ad70d783982d37d2ac9
Eric L <elezotte@gmail.com>
2019-04-18 15:43:02 -0600

    3.39.0 release notes
    Change-Id: I20f0b9ce71939c211a4e6303db2eda0089adc3b0

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.39.0",
+        file: require("./release-notes/v3-39-0.md")
+    },
+    {
         title: "v3.38.0",
         file: require("./release-notes/v3-38-0.md")
     },
