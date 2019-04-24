bab564780493c80e6ff2f36e008b37ce0d8f9476
Eric Lezotte <elezotte@pingidentity.com>
2019-04-24 07:24:47 -0600

    3.40.0 release notes
    Change-Id: Id7fc51ea8c8d1173a99a3b34c14ded2d5bd9b8ba

--- packages/ui-library/src/demo/components/docs/notes.js
+++ packages/ui-library/src/demo/components/docs/notes.js
@@ -1,5 +1,9 @@
 const nodes = [
     {
+        title: "v3.40.0",
+        file: require("./release-notes/v3-40-0.md")
+    },
+    {
         title: "v3.39.0",
         file: require("./release-notes/v3-39-0.md")
     },
