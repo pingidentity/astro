bf56fad608531445747b89b74225b5dd1eaeec3a
Remy Pearlstone <remypearlstone@pingidentity.com>
2019-10-04 14:18:35 -0600

    different aligment

--- packages/ui-library/src/components/buttons/TileSelector.jsx
+++ packages/ui-library/src/components/buttons/TileSelector.jsx
@@ -77,7 +77,7 @@ const selectorTypes = {
 const getTilePanel = () => {
     const tileSelector = document.querySelector(".tile-button--selected");
     if (tileSelector) {
-        return document.querySelector(".tile-panel").scrollIntoView({ behavior: "smooth" });
+        return document.querySelector(".tile-panel").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
     }
 };
 
