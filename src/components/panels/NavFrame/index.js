a26a57872273cd62e8a628d7473af7d21a6e9cd8
Tyler Grove <tgrove@pingidentity.com>
2019-10-15 14:57:53 -0600

    move NavFrame into UI Library folder

--- src/components/panels/NavFrame/index.js
+++
@@ -1,9 +0,0 @@
-import NavFrame from "./NavFrame";
-export default NavFrame;
-export {
-    EnvironmentSelector,
-    MarketSelector,
-    NavLink,
-    NavMenu
-} from "./NavHeader";
-export { default as NavSearch } from "./NavSearch";
