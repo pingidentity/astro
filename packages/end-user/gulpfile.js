392332e53117ccbef1d975bffd54ae374dbcc247
Tyler Grove <tgrove@pingidentity.com>
2019-10-18 14:11:13 -0600

    Remove makefile and move logic to gulpfile update and clean up jenkinsfile

--- packages/end-user/gulpfile.js
+++ packages/end-user/gulpfile.js
@@ -2,23 +2,32 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const del = require('del');
 const debug = require('gulp-debug');
+const log = require('fancy-log');
 
+const paths = {
+    dist: 'dist',
+    cdn: 'cdn',
+}
 
-gulp.task('delete-dist', () => del(['dist']));
+gulp.task('delete-dist', () => del([paths.dist]));
+
+gulp.task('delete-cdn', () => del([paths.cdn]));
 
 gulp.task('transpile-components', () =>
     gulp
         .src(['./src/components**/**/*(*.jsx|*.js)', '!./src/components**/**/*(*.test.jsx|*.test.js)'])
         .pipe(babel())
         .pipe(debug({ title: 'transpiling:' }))
-        .pipe(gulp.dest('dist')));
+        .pipe(gulp.dest(paths.dist))
+);
 
 gulp.task('transpile-utils', () =>
     gulp
         .src(['./src/util**/**/*(*.jsx|*.js)'])
         .pipe(babel())
         .pipe(debug({ title: 'transpiling utils:' }))
-        .pipe(gulp.dest('dist')));
+        .pipe(gulp.dest(paths.dist))
+);
 
 gulp.task('move-files', () =>
     gulp
@@ -27,15 +36,35 @@ gulp.task('move-files', () =>
             './package.json',
             './static/end-user.css',
             './src/css**/**/*',
-            './static/*(*.svg|*.map|*.jpg|*.otf)',
+            './static/*',
+            '!./static/*.html'
         ])
         .pipe(debug({ title: 'moving:' }))
-        .pipe(gulp.dest('dist')));
+        .pipe(gulp.dest(paths.dist))
+);
+
+gulp.task('build-cdn', () =>
+    gulp
+        .src([
+            './static/*',
+            '!./static/*.html'
+        ])
+        .pipe(debug({ title: 'perparing for cdn:' }))
+        .pipe(gulp.dest(paths.cdn))
+        .on('end', () => { log(`cdn built`);} )
+);
 
 gulp.task('build-dist', gulp.series(
     'delete-dist',
     'transpile-components',
     'transpile-utils',
     'move-files',
+    done => done(),
+));
+
+gulp.task(paths.dist, gulp.series(
+    'build-dist',
+    'delete-cdn',
+    'build-cdn',
     done => done(),
 ));
