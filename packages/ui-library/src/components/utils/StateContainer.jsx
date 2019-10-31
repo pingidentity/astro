11b9698abc484d91d732a0356e5b04197e2db6ee
Gwyneth Rose <gwynethrose@pingidentity.com>
2019-10-30 20:56:15 -0600

    Fixed state container issue

--- packages/ui-library/src/components/utils/StateContainer.jsx
+++ packages/ui-library/src/components/utils/StateContainer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Utils from "../../util/Utils";
-import { cannonballChangeWarning } from "../../util/DeprecationUtils";
 
 const mapNothing = () => ({});
 
