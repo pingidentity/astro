4c5490142c72e0276aeb41f435b6489453e2921e
Tyler Grove <tgrove@pingidentity.com>
2019-11-21 16:35:11 -0700

    update static file

--- packages/end-user/webpack.static.js
+++ packages/end-user/webpack.static.js
@@ -1,21 +1,10 @@
 const merge = require('webpack-merge');
 
-const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
-const common = require('./webpack.common.js');
+const common = require('./webpack.dev.js');
 
 module.exports = merge(common, {
     output: {
         path: `${__dirname}/build`,
         filename: '[name].bundle.js',
-        libraryTarget: 'umd',
     },
-    target: 'node',
-    module: {
-        rules: [{
-            test: /\.html$/,
-            loader: 'raw-loader',
-        }],
-    },
-    plugins: [new StaticSiteGeneratorPlugin()],
-    entry: common.entry.main = './src/demo/static.js',
 });
