34c2cf74a56f8bd6a4a8c253f6fe339623194da3
Noah Buscher <noahbuscher@pingidentity.com>
2019-12-04 21:35:02 +0000

    UIP-3050: Add edit screen and fix FloatLabel TextInputs [end-user]

---
+++ packages/end-user/src/components/Columns/Columns.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const alignments = {
+    LEFT: 'left',
+    CENTER: 'center',
+    RIGHT: 'right',
+};
+
+const widths = {
+    ONE: 'one',
+    TWO: 'two',
+    THREE: 'three',
+    FOUR: 'four',
+    FIVE: 'five',
+    SIX: 'six',
+    SEVEN: 'seven',
+    EIGHT: 'eight',
+    NINE: 'nine',
+    TEN: 'ten',
+    ELEVEN: 'eleven',
+    TWELVE: 'twelve',
+};
+
+const Columns = ({
+    children,
+}) => {
+    return (
+        <div className="columns">
+            {children}
+        </div>
+    );
+};
+
+const Column = ({
+    className,
+    'data-id': dataId,
+    children,
+    width,
+    alignment,
+}) => {
+    const classNames = classnames(className, `column__${width}`, {
+        'column--left': alignment === alignments.LEFT,
+        'column--center': alignment === alignments.CENTER,
+        'column--right': alignment === alignments.RIGHT,
+    });
+
+    return (
+        <div className={classNames} data-id={dataId}>
+            {children}
+        </div>
+    );
+};
+
+Column.propTypes = {
+    'data-id': PropTypes.string,
+    className: PropTypes.string,
+    width: PropTypes.oneOf(Object.values(widths)),
+    alignments: PropTypes.oneOf(Object.values(alignments)),
+};
+
+Column.defaultProps = {
+    width: widths.ONE,
+    alignment: alignments.LEFT,
+};
+
+Columns.Column = Column;
+Columns.alignments = alignments;
+Columns.widths = widths;
+export default Columns;
