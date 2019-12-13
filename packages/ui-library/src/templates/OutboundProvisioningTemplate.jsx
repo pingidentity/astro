b9b2e069fc3f112ab96caa67f0f8a9e61b8b9758
Allegra Weldon <allegraweldon@pingidentity.com>
2019-12-13 12:26:03 -0700

    UIP-3071 correcting styling lint

--- packages/ui-library/src/templates/OutboundProvisioningTemplate.jsx
+++ packages/ui-library/src/templates/OutboundProvisioningTemplate.jsx
@@ -49,9 +49,6 @@ const initialState = {
     attributes: [],
     description: "",
     name: "",
-<<<<<<< Updated upstream
-    selectedValue: "",
-=======
     dropDownOptions: [
         { label: "phone number", value: "1" },
         { label: "email", value: "2" },
@@ -59,8 +56,6 @@ const initialState = {
         { label: "given name", value: "4" },
     ],
     selectedValue: "",
-    // dropDownSelectedValue: { label: "phone number", value: "1" },
->>>>>>> Stashed changes
 };
 
 
@@ -83,10 +78,6 @@ const possibleProviders = {
     }
 };
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 const AttributesFirstRow = (
     [<Row key="first">
         <FormTextField
@@ -97,10 +88,7 @@ const AttributesFirstRow = (
         <FormTextField
             width={InputWidths.MD}
             value="username"
-<<<<<<< Updated upstream
             required
-=======
->>>>>>> Stashed changes
         />
     </Row>]
 );
@@ -115,10 +103,7 @@ const AttributesSecondRow = (
         <FormTextField
             width={InputWidths.MD}
             value="given name"
-<<<<<<< Updated upstream
             required
-=======
->>>>>>> Stashed changes
         />
     </Row>]
 );
@@ -133,10 +118,7 @@ const AttributesThirdRow = (
         <FormTextField
             width={InputWidths.MD}
             value="family name"
-<<<<<<< Updated upstream
             required
-=======
->>>>>>> Stashed changes
         />
     </Row>]
 );
@@ -151,10 +133,7 @@ const AttributesFourthRow = (
         <FormTextField
             width={InputWidths.MD}
             value="username"
-<<<<<<< Updated upstream
             required
-=======
->>>>>>> Stashed changes
         />
     </Row>]
 );
@@ -169,15 +148,11 @@ const AttributesFifthRow = (
         <FormTextField
             width={InputWidths.MD}
             value="email"
-<<<<<<< Updated upstream
             required
-=======
->>>>>>> Stashed changes
         />
     </Row>]
 );
 
-<<<<<<< Updated upstream
 class AttributesSixthRow extends Component {
     state = {
         dropDownSelectedValue: {},
@@ -238,43 +213,12 @@ class AttributesSixthRow extends Component {
         );
     }
 }
-=======
-const AttributesSixthRow = ({
-    dropDownOptions,
-    handleDropDownValueChange,
-    dropDownSelectedValue,
-}) => {
-    return (
-        [<Row key="sixth">
-            <FormTextField
-                width={InputWidths.MD}
-                value="Email Encoding Key"
-            />
-            <Separator>=</Separator>
-            <FormDropDownList
-                options={dropDownOptions}
-                canAdd={true}
-                //onAdd={this._handleAdd7}
-                labelAdd="ADD"
-                labelPrompt="Type to search or add"
-                selectedOption={dropDownSelectedValue}
-                onValueChange={handleDropDownValueChange}
-                width={InputWidths.MD}
-            />
-        </Row>]
-    );
-};
->>>>>>> Stashed changes
 
 const ProfileEdit = ({
     setStepState,
     name,
     description,
-<<<<<<< Updated upstream
     activeProvider,
-=======
-    logo,
->>>>>>> Stashed changes
 }) => {
     return (
         <PageSection>
@@ -300,7 +244,6 @@ const ProfileEdit = ({
                     label="Logo"
                     labelSelect="Choose a File"
                     showThumbnail
-<<<<<<< Updated upstream
                     labelRemove=""
                     thumbnailSrc={
                         activeProvider.customLogo ? activeProvider.customLogo : (<div>
@@ -317,9 +260,6 @@ const ProfileEdit = ({
                             </div>
                         </div>)
                     }
-=======
-                    thumbnailSrc={logo || ""}
->>>>>>> Stashed changes
                 />
             </InputRow>
         </PageSection>
@@ -395,7 +335,6 @@ class AuthorizationEdit extends Component {
     deprovisioningDropDownChange = this.setDropDown("deprovisioningDropDownOption")
     permissionDropDownChange = this.setDropDown("permissionDropDownOption")
 
-<<<<<<< Updated upstream
     setChecked = checkedName => () => this.setState(
         ({ [checkedName]: checked }) => ({ [checkedName]: !checked })
     )
@@ -409,17 +348,6 @@ class AuthorizationEdit extends Component {
 
         return (
             <PageSection>
-=======
-const AttributesEdit = (props) => {
-
-    return (
-        <div>
-            <PageSection
-                title={
-                    `Link ${props.activeProvider.label}'s attributes to your Pingone Identity attributes.`
-                }
-            >
->>>>>>> Stashed changes
                 <InputRow>
                     <FormDropDownList
                         label="Deprovisioning Action"
@@ -465,10 +393,6 @@ const AttributesEdit = (props) => {
                     <InputRow>
                         <RowBuilder
                             hasLineBetween={false}
-<<<<<<< Updated upstream
-=======
-                            onAdd={props.addRow("first")}
->>>>>>> Stashed changes
                             rows={[
                                 {
                                     id: "first",
@@ -497,11 +421,7 @@ const AttributesEdit = (props) => {
                                 },
                                 {
                                     id: "sixth",
-<<<<<<< Updated upstream
                                     content: <AttributesSixthRow setStepState={props.activeProvider.setStepState} />,
-=======
-                                    content: <AttributesSixthRow setStepState={props.activeProvider.setStepState} dropDownOptions={props.activeProvider.dropDownOptions} handleDropDownValueChange={props.handleDropDownValueChange} dropDownSelectedValue={props.activeProvider.dropDownSelectedValue} />,
->>>>>>> Stashed changes
                                     removable: false
                                 },
                             ]}
@@ -846,40 +766,10 @@ export default class OutboundProvisioning extends Component {
             "population1"
         ],
         privacyType: "1",
-<<<<<<< Updated upstream
         dropDownSelectedValue: { label: "phone number", value: "1" },
     }
 
     
-=======
-        firstRowIds: [uuidV4(), uuidV4()],
-        secondRowIds: [uuidV4(), uuidV4()],
-        thirdRowIds: [uuidV4()],
-        dropDownSelectedValue: { label: "phone number", value: "1" },
-    }
-
-    handleDropDownValueChange = selectedOption => {
-        this.setState({
-            dropDownSelectedValue: { selectedOption }
-        });
-        console.log(this.state.dropDownSelectedValue);
-    };
-    
-    
-
-    _handleAdd7 = (optionLabel) => {
-        // Mock new option
-        const newOption = { label: optionLabel, value: optionLabel };
-        const newOptions = this.state.dropDownOptions.concat([newOption]);
-
-        // Update options & select the newly added option
-        this.setState({
-            dropDownOptions: newOptions,
-            selectedValue7: newOption
-        });
-    };
-
->>>>>>> Stashed changes
     addRow = key => () => {
         const stateKey = `${key}RowIds`;
         this.setState(state => ({
