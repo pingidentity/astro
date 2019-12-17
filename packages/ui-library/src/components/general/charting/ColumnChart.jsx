886c101375f8847cfba7d6d4641a73ca9dcaaceb
Gwyneth Rose <gwynethrose@pingidentity.com>
2019-12-17 04:30:11 +0000

    UIP-3114: Set up ColumnChart so that tooltip content can be formatted

--- packages/ui-library/src/components/general/charting/ColumnChart.jsx
+++ packages/ui-library/src/components/general/charting/ColumnChart.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { BarChart, Bar, XAxis, Cell, Tooltip, ReferenceLine, CartesianGrid } from "recharts";
+import { BarChart, Bar, XAxis, Cell, Tooltip, ReferenceLine, ResponsiveContainer, CartesianGrid } from "recharts";
 import _ from "underscore";
+import classnames from "classnames";
+
 import Color from "color";
-import Legend from "./Legend";
+import DashboardCardTitle from "./Cards/DashboardCardTitle";
+import { LegendItem } from "./Legend";
+import { defaultRender } from "../../../util/PropUtils";
 
 /**
  * @class ColumnChart
@@ -42,14 +46,34 @@ import Legend from "./Legend";
  *     The event that triggered the callback.
 */
 
+/**
+ * Function that can be used to change how the tooltip renders. For example,
+ * showing a value as $38 instead of just 38.
+ * @callback ColumnChart~renderTooltip
+ * @param {Object} props
+ *     The props of the default component; these can be spread into the component.
+ * @param {Object} defaultComponent
+ *     The component that the ColumnChart renders by default.
+*/
+
+
+export const ColumnChartTitle = ({ className, ...props }) => (
+    <DashboardCardTitle
+        {...props}
+        className={classnames(className, "dashboard-card__title--horizontal-bar-card")}
+    />
+);
+
 export default class ColumnChart extends React.Component {
     static propTypes = {
+        className: PropTypes.string,
         data: PropTypes.arrayOf(
             PropTypes.shape({
                 id: PropTypes.string,
                 data: PropTypes.arrayOf(PropTypes.number)
             })
         ),
+        height: PropTypes.number,
         onMouseOver: PropTypes.func,
         onMouseOut: PropTypes.func,
         legend: PropTypes.arrayOf(PropTypes.shape({
@@ -57,15 +81,20 @@ export default class ColumnChart extends React.Component {
             color: PropTypes.string
         })),
         referenceLineColor: PropTypes.string,
+        renderTooltip: PropTypes.func,
+        width: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
     };
 
     static defaultProps = {
         "data-id": "column-chart",
         data: [],
+        height: 300,
         legend: [],
         onMouseOver: _.noop,
         onMouseOut: _.noop,
         referenceLineColor: "#57A0EA",
+        renderTooltip: defaultRender,
+        width: "100%"
     };
 
     state = {
@@ -115,19 +144,17 @@ export default class ColumnChart extends React.Component {
             return;
         }
 
-        const d = [
-            {
-                color: this.state.legend[this.state.selected.y.index].color,
-                label: this.state.selected.y.label,
-                value: this.props.data
-                    .find(o => o.id === this.state.selected.x.label).data
-                    .find(o => o.id === this.state.selected.y.label).value
-            }
-        ];
+        const data = {
+            color: this.state.legend[this.state.selected.y.index].color,
+            label: this.state.selected.y.label,
+            value: this.props.data
+                .find(o => o.id === this.state.selected.x.label).data
+                .find(o => o.id === this.state.selected.y.label).value
+        };
 
         return (
             <div className="column-chart__tooltip">
-                <Legend data={d} />
+                {this.props.renderTooltip(data, LegendItem)}
             </div>
         );
     };
@@ -142,68 +169,72 @@ export default class ColumnChart extends React.Component {
         const digestedData = this._digestData(data);
 
         return (
-            <BarChart
-                width={550}
-                height={300}
-                data={digestedData}
-                margin={{
-                    top: 20, right: 30, left: 20, bottom: 5,
-                }}
-                className="column-chart"
-                data-id={dataId}
+            <ResponsiveContainer
+                className={this.props.className}
+                height={this.props.height}
+                width={this.props.width}
             >
-                <XAxis dataKey="name" hide={true} />
-                <CartesianGrid vertical={false} />
-                <Tooltip
-                    isAnimationActive={false}
-                    content={this._renderTooltip}
-                    cursor={false}
-                />
-                {
-                    legend.map(({ id, label, color }, key) => {
-                        return (
-                            <Bar
-                                label={label ? label : id}
-                                dataKey={key}
-                                key={id}
-                                stackId="a"
-                                maxBarSize={60}
-                                fill={color}
-                                onMouseOver={this._handleMouseOver(id)}
-                                onMouseOut={this._handleMouseOut}
-                                radius={key === legend.length - 1 ? [3, 3, 0, 0] : null}
-                            >
-                                {
-                                    digestedData.map((item) => (
-                                        <Cell
-                                            key={item.name}
-                                            className="column-chart__cell"
-                                            style={{
-                                                stroke: this.state.selected.x &&
-                                                    this.state.selected.y &&
-                                                    this.state.selected.x.label === item.name &&
-                                                    this.state.selected.y.label === id
-                                                    ? Color(color).lighten(0.5)
-                                                    : color,
-                                                strokeWidth: "1px",
-                                            }}
-                                        />
-                                    ))}
-                            </Bar>
-                        );
-                    })
-                }
-                <ReferenceLine
-                    x={this.state.selected.x ? this.state.selected.x.label : null}
-                    stroke="#57A0EA"
-                    label={{
-                        position: "top",
-                        value: this.state.selected.x ? this.state.selected.x.label : null,
-                        fill: this.props.referenceLineColor,
-                        fontSize: 14
+                <BarChart
+                    data={digestedData}
+                    margin={{
+                        top: 20, right: 30, left: 20, bottom: 5,
                     }}
-                />
-            </BarChart>
+                    className="column-chart"
+                    data-id={dataId}
+                >
+                    <XAxis dataKey="name" hide={true} />
+                    <CartesianGrid vertical={false} />
+                    <Tooltip
+                        isAnimationActive={false}
+                        content={this._renderTooltip}
+                        cursor={false}
+                    />
+                    {
+                        legend.map(({ id, label, color }, key) => {
+                            return (
+                                <Bar
+                                    label={label ? label : id}
+                                    dataKey={key}
+                                    key={id}
+                                    stackId="a"
+                                    maxBarSize={60}
+                                    fill={color}
+                                    onMouseOver={this._handleMouseOver(id)}
+                                    onMouseOut={this._handleMouseOut}
+                                    radius={key === legend.length - 1 ? [3, 3, 0, 0] : null}
+                                >
+                                    {
+                                        digestedData.map((item) => (
+                                            <Cell
+                                                key={item.name}
+                                                className="column-chart__cell"
+                                                style={{
+                                                    stroke: this.state.selected.x &&
+                                                        this.state.selected.y &&
+                                                        this.state.selected.x.label === item.name &&
+                                                        this.state.selected.y.label === id
+                                                        ? Color(color).lighten(0.5)
+                                                        : color,
+                                                    strokeWidth: "1px",
+                                                }}
+                                            />
+                                        ))}
+                                </Bar>
+                            );
+                        })
+                    }
+                    <ReferenceLine
+                        x={this.state.selected.x ? this.state.selected.x.label : null}
+                        stroke="#57A0EA"
+                        label={{
+                            position: "top",
+                            value: this.state.selected.x ? this.state.selected.x.label : null,
+                            fill: this.props.referenceLineColor,
+                            fontSize: 14
+                        }}
+                    />
+                </BarChart>
+            </ResponsiveContainer>
         );
     }
 }
