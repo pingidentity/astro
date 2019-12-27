659aeb7a9d301fa6a8c563c688b374209c5957df
Noah Buscher <noahbuscher@pingidentity.com>
2019-12-27 21:14:48 +0000

    UIP-3131: Fix Legend alignments

--- packages/ui-library/src/components/general/charting/tests/LegendTests.jsx
+++ packages/ui-library/src/components/general/charting/tests/LegendTests.jsx
@@ -1,44 +1,81 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Legend, { LegendItem } from "../Legend";
+import Legend, { boxAlignments, LegendItem } from "../Legend";
 
 describe("Legend", () => {
+    const defaultProps = {
+        data: [
+            {
+                id: "1D",
+                helpText: "This is by day",
+                data: [
+                    {
+                        id: "November 11, 2019",
+                        data: [
+                            60,
+                            20,
+                        ]
+                    },
+                    {
+                        id: "November 12, 2019",
+                        data: [
+                            20,
+                            30,
+                        ]
+                    },
+                    {
+                        id: "November 13, 2019",
+                        data: [
+                            40,
+                            60,
+                        ]
+                    },
+                ]
+            }
+        ]
+    };
+
     it("renders the component", () => {
         const component = shallow(
             <Legend
-                data={[
-                    {
-                        id: "1D",
-                        helpText: "This is by day",
-                        data: [
-                            {
-                                id: "November 11, 2019",
-                                data: [
-                                    60,
-                                    20,
-                                ]
-                            },
-                            {
-                                id: "November 12, 2019",
-                                data: [
-                                    20,
-                                    30,
-                                ]
-                            },
-                            {
-                                id: "November 13, 2019",
-                                data: [
-                                    40,
-                                    60,
-                                ]
-                            },
-                        ]
-                    }
-                ]}
+                {...defaultProps}
             />
         );
 
         expect(component.exists()).toEqual(true);
+    });
+
+    it("renders with correct left boxAlignment class", () => {
+        const component = shallow(
+            <Legend
+                {...defaultProps}
+                boxAlignment={boxAlignments.LEFT}
+            />
+        );
+
+        expect(component.find(".legend--left").exists()).toEqual(true);
+    });
+
+    it("renders with correct center boxAlignment class", () => {
+        const component = shallow(
+            <Legend
+                {...defaultProps}
+                boxAlignment={boxAlignments.CENTER}
+            />
+        );
+
+        expect(component.find(".legend--center").exists()).toEqual(true);
+    });
+
+    it("renders with correct right boxAlignment class", () => {
+        const component = shallow(
+            <Legend
+                {...defaultProps}
+                boxAlignment={boxAlignments.RIGHT}
+            />
+        );
+
+        expect(component.find(".legend--right").exists()).toEqual(true);
     });
 
     describe("LegendItem", () => {
