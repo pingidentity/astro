2ed85838cfac3c587854893d47c12be0ffcd0f73
Tyler Grove <tgrove@pingidentity.com>
2020-01-18 18:54:14 +0000

    UIP-3175-portal-in-modal-is-not-interactable

--- packages/ui-library/src/util/tests/EventUtilsTests.jsx
+++ packages/ui-library/src/util/tests/EventUtilsTests.jsx
@@ -2,11 +2,51 @@ window.__DEV__ = true;
 
 jest.dontMock("../EventUtils");
 
-describe("ReduxUtils", function () {
+describe("EventUtils", function () {
     var React = require("react"),
         ReactDOM = require("react-dom"),
         ReactTestUtils = require("react-dom/test-utils"),
         Utils = require("../EventUtils");
+
+    // This component mocks a body, modal and portal to test the click event
+    class MockComponent extends React.Component {
+        constructor(props) {
+            super(props);
+            this.component = React.createRef();
+            this.outsideModal = React.createRef();
+            this.modal = React.createRef();
+            this.insideModal = React.createRef();
+            this.portal = React.createRef();
+            this.insidePortal = React.createRef();
+        }
+        //Mock globals to see if we clicked outside of modal
+        mockGlobalClick = e => {
+            Utils.callIfOutsideOfContainer(this.modal.current, this.props.callback, e);
+            e.stopPropagation();
+        };
+        render() {
+            return (
+                <div ref={this.component}>
+                    <div ref={this.outsideModal} onClick={this.mockGlobalClick}>
+                        <div ref={this.modal} onClick={this.mockGlobalClick}>
+                            <div ref={this.insideModal} onClick={this.mockGlobalClick} data-id="portal-trigger" />
+                        </div>
+                    </div>
+                    <div ref={this.portal} data-parent="portal-trigger">
+                        <div ref={this.insidePortal} onClick={this.mockGlobalClick}/>
+                    </div>
+                </div>
+            );
+        }
+    }
+    // neither test utils or enzyme acutally attach to document so use this to test because call if outside of container expects element to be in document.body
+    const attachComponentToDocument = Component => {
+        const div = document.createElement("div");
+        document.body.appendChild(div);
+
+        return ReactDOM.render(Component, div);
+    };
+
 
     beforeEach(function() {
         global.getSelection = jest.fn();
@@ -42,30 +82,33 @@ describe("ReduxUtils", function () {
         expect(callback).toBeCalledWith(105);
     });
 
-    it("Calls callback only if outside", function () {
-        class MockComp extends React.Component {
-            _handleClick = (e) => {
-                Utils.callIfOutsideOfContainer(
-                    ReactDOM.findDOMNode(this.refs.inner),
-                    this.props.onClick, e);
-            };
+    it("Calls callback if outside", function () {
+        const clickedOutside = jest.fn();
 
-            render() {
-                return (
-                    <div ref="outer" onClick={this._handleClick}>
-                        <div ref="inner" onClick={this._handleClick}>
-                            <div ref="innerMost" onClick={this._handleClick} />
-                        </div>
-                    </div>);
-            }
-        }
+        const component = attachComponentToDocument(<MockComponent callback={clickedOutside} />);
+        ReactTestUtils.Simulate.click(component.outsideModal.current);
 
-        var callback = jest.fn();
-        var component = ReactTestUtils.renderIntoDocument(<MockComp onClick={callback} />);
+        expect(clickedOutside).toBeCalled();
+    });
 
-        ReactTestUtils.Simulate.click(component.refs.innerMost);
-        expect(callback).not.toBeCalled();
-        callback.mockClear();
+    it("Doesn't call callback if inside", function () {
+        const cco = jest.fn();
+
+        const component = attachComponentToDocument(<MockComponent callback={cco} />);
+
+        ReactTestUtils.Simulate.click(component.insideModal.current);
+
+        expect(cco).not.toBeCalled();
+    });
+
+    it("Doesn't call callback if data-parent is present on element", function () {
+        const clickedOutside = jest.fn();
+
+        const component = attachComponentToDocument(<MockComponent callback={clickedOutside} />);
+
+        ReactTestUtils.Simulate.click(component.insidePortal.current);
+
+        expect(clickedOutside).not.toBeCalled();
     });
 
     it("does not call call back when text selected", function () {
