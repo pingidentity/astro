e706de7cf93c134756dc6e77651f59ac4c7d6b92
Remy Pearlstone <remypearlstone@pingidentity.com>
2020-01-24 12:42:56 -0700

    UIP-3224-more-disabled-demos

--- packages/ui-library/src/demo/components/layout/DisabledDemo.jsx
+++ packages/ui-library/src/demo/components/layout/DisabledDemo.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Disabled from "ui-library/lib/components/layout/Disabled";
+import Button from "../../../components/buttons/Button";
+import FormCheckbox from "./../../../components/forms/FormCheckbox";
+import ExpandableRow from "ui-library/lib/components/rows/ExpandableRow";
 
 /**
 * @name DisabledDemo
@@ -25,6 +28,37 @@ const DisabledDemo = () => (
             this is disabled text with a helphint
             this is disabled text with a helphint
         </Disabled>
+        <Disabled>
+            <br />
+            <br />
+            <Button
+                label="Add"
+                iconName="add"
+            />
+            <Button
+                iconName="edit"
+                inline
+            />
+        </Disabled>
+        <br />
+        <br />
+        <Disabled>
+            <FormCheckbox
+                label = {"Regular Checkbox; checked: "}
+                value = ""
+                inline
+            />
+        </Disabled>
+        <br />
+        <br />
+        <Disabled>
+            <ExpandableRow
+                title="Basic Row"
+                subtitle="Row Subtitle"
+            />
+        </Disabled>
+
+
     </div>
 );
 
