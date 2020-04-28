ec0e9fda05da95468e1e9a78025b0922b5aa6aee
Noah Buscher <noahbuscher@pingidentity.com>
2020-04-28 05:36:02 +0000

    UIP-3327: Fix FloatLabel x-browser

--- packages/end-user/src/components/FloatLabel/FloatLabel.js
+++ packages/end-user/src/components/FloatLabel/FloatLabel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -12,20 +12,45 @@ import classnames from 'classnames';
  *      Sets a data-id property on the FloatLabel element to be used as a test hook
  * @param {string} [id]
  *      ID for the input element
- * @param {node} [InputType]
+ * @param {node} [input]
  *      Sets the input element to be used
- * @param {string} [inputClassName]
- *      Sets the classname for the input
- *
  */
 const FloatLabel = ({
-    label, id, InputType, inputClassName, children, 'data-id': dataId, ...props
+    label,
+    id,
+    input,
+    onChange,
+    inputClassName,
+    defaultValue,
+    value,
+    children,
+    InputType,
+    'data-id': dataId,
+    ...props
 }) => {
+    const [inputValue, setInputValue] = useState(value || defaultValue);
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+        if (props.onChange) props.onChange(e);
+    };
+
+    const floatClassNames = classnames('float-label', {
+        'placeholder-shown': !inputValue
+    });
     const inputClassNames = classnames('float-label__input', inputClassName);
 
     return (
-        <div className="float-label" data-id={dataId}>
-            <InputType className={inputClassNames} placeholder={label} id={id} {...props} />
+        <div className={floatClassNames} data-id={dataId}>
+            <InputType
+                id={id}
+                placeholder={label}
+                defaultValue={defaultValue}
+                value={value}
+                onChange={handleChange}
+                className={inputClassNames}
+                {...props}
+            />
             <label className="float-label__label" htmlFor={id}>
                 {label}
             </label>
--- packages/end-user/src/components/FloatLabelTextInput/FloatLabelTextInput.js
+++ packages/end-user/src/components/FloatLabelTextInput/FloatLabelTextInput.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FloatLabel from '../FloatLabel';
 import TextInput from '../TextInput';
+import { noop } from "underscore";
 
 /**
  * TextInput with a FloatLabel
  */
-const FloatLabelTextInput = props => <FloatLabel {...props} InputType={TextInput} />;
+const FloatLabelTextInput = ({
+    onChange,
+    value,
+    defaultValue,
+    inputClassName,
+    label,
+    id,
+    ...props
+}) => {
+    return (
+        <FloatLabel
+            defaultValue={defaultValue}
+            label={label}
+            id={id}
+            InputType={TextInput}
+            inputClassName={inputClassName}
+            onChange={onChange}
+            value={value}
+            {...props}
+        />
+    );
+};
+
+FloatLabelTextInput.propTypes = {
+    /**
+     * Default value for the input
+     */
+    defaultValue: PropTypes.string,
+    /**
+     * ID to add to the input
+     */
+    id: PropTypes.string,
+    /**
+     * Class name(s) to add to the input
+     */
+    inputClassName: PropTypes.string,
+    /**
+     * Label for the input
+     */
+    label: PropTypes.string,
+    /**
+     * Called when the value of the input changes
+     */
+    onChange: PropTypes.func,
+    /**
+     * Value of the input
+     */
+    value: PropTypes.string,
+};
+
+FloatLabelTextInput.defaultProps = {
+    onChange: noop,
+};
 
 export default FloatLabelTextInput;
