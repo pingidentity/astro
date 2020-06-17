11cbb3abe4d9f1aa81a9ee3b3d9e4ccf39e036a3
Julius Williams <juliuswilliams@pingidentity.com>
2020-06-17 12:49:24 -0600

    UIP-3686: Float label textarea

---
+++ packages/end-user/src/components/FloatLabelTextArea/FloatLabelTextArea.story.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FloatLabelTextArea from './FloatLabelTextArea';
+
+export default {
+    title: 'Components/Inputs/TextArea/Float',
+    component: FloatLabelTextArea,
+};
+
+export const Default = () => (
+    <FloatLabelTextArea
+        label='Float label text area'
+    />
+);
--- packages/end-user/src/components/TextArea/TextArea.story.js
+++ packages/end-user/src/components/TextArea/TextArea.story.js
@@ -3,7 +3,7 @@ import TextArea from './TextArea';
 
 
 export default {
-    title: 'Components/Inputs/TextArea',
+    title: 'Components/Inputs/TextArea/Basic',
     component: TextArea,
 };
 
