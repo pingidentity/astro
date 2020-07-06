4f2a0b60a6c4ae1f914c565c13e30dfe0c473d10
Eli Cooper <elijahcooper@pingidentity.com>
2020-07-06 12:24:21 -0500

    UIP-3754: Lighten the error and warning colors for field messages

--- packages/end-user/src/components/FieldMessage/FieldMessage.story.js
+++ packages/end-user/src/components/FieldMessage/FieldMessage.story.js
@@ -4,37 +4,45 @@ import { fieldMessageStatuses } from './FieldMessage';
 
 export default {
     title: 'Components/Inputs/FieldMessage',
+    decorators: [],
     component: TextInput,
 };
 
-export const Default = () => (<React.Fragment>
+export const Default = () => (
     <TextInput
         placeholder="Text"
         fieldMessage="Here is a default message"
     />
+);
+
+export const Error = () => (
     <TextInput
         placeholder="Error"
         fieldMessage="Here is an error message"
         fieldMessageProps={{ status: fieldMessageStatuses.ERROR }}
     />
+);
+
+export const Info = () => (
     <TextInput
         placeholder="Info"
-        fieldMessage="Here is a success message"
+        fieldMessage="Here is an info message"
         fieldMessageProps={{ status: fieldMessageStatuses.INFO }}
     />
+);
+
+export const Success = () => (
     <TextInput
         placeholder="Success"
         fieldMessage="Here is a success message"
         fieldMessageProps={{ status: fieldMessageStatuses.SUCCESS }}
     />
+);
+
+export const Warning = () => (
     <TextInput
         placeholder="Warning"
-        fieldMessage="Here is a success message"
+        fieldMessage="Here is a warning message"
         fieldMessageProps={{ status: fieldMessageStatuses.WARNING }}
     />
-    <TextInput
-        placeholder="Primary"
-        fieldMessage="Here is a primary (default) message"
-        fieldMessageProps={{ status: fieldMessageStatuses.DEFAULT }}
-    />
-</React.Fragment>);
+);
