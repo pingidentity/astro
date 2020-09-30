2c60d1f47ec2aa5e0d8e2fa361b21b51cd568a75
Elijah Cooper <elijahcooper@pingidentity.com>
2020-09-30 20:13:19 +0000

    UIP-3905: Astro button

---
+++ packages/astro/src/components/Button/Button.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useFocusRing } from '@react-aria/focus';
+import { render, screen } from '@testing-library/react';
+import Button from '.';
+
+jest.mock('@react-aria/focus', () => ({
+  ...jest.requireActual('@react-aria/focus'),
+  useFocusRing: jest.fn(),
+}));
+
+const testId = 'test-button';
+const defaultProps = {
+  'data-testid': testId,
+};
+const getComponent = (props = {}) => render(
+  <Button {...defaultProps} {...props}>
+    {props.children}
+  </Button>,
+);
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+test('default button', () => {
+  useFocusRing.mockImplementation(() => ({ isFocusVisible: false, focusProps: {} }));
+  getComponent();
+  const button = screen.getByTestId(testId);
+  expect(button).toBeInstanceOf(HTMLButtonElement);
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveStyleRule('box-shadow', 'none', { target: ':focus' });
+});
+
+test('button with focus', () => {
+  useFocusRing.mockImplementation(() => ({ isFocusVisible: true, focusProps: {} }));
+  getComponent();
+  const button = screen.getByTestId(testId);
+  expect(button).toHaveStyleRule('box-shadow', 'focus', { target: ':focus' });
+});
