334b64fdf21bc8d0b7981e813cfab5bbe3e48cf8
Elijah Cooper <elijahcooper@pingidentity.com>
2020-10-01 16:27:26 +0000

    UIP-3910: Astro checkbox

---
+++ packages/astro/src/components/Checkbox/Checkbox.js
@@ -0,0 +1,18 @@
+import React, { forwardRef } from 'react';
+import { Checkbox as RCheckbox } from '@rebass/forms';
+
+/**
+ * Basic checkbox input.
+ * Built on top of the [Checkbox from Rebass Forms](https://rebassjs.org/forms/checkbox) and
+ * uses the available [props from Rebass](https://rebassjs.org/props/).
+ *
+ * **Note: Requires a label. It's recommended to use `CheckboxField` for a complete solution.**
+ */
+const Checkbox = forwardRef((props, ref) => (
+  <RCheckbox
+    ref={ref}
+    {...props}
+  />
+));
+
+export default Checkbox;
