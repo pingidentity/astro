a713c32dcb854278f0450e4e1c4714fe62a76313
Tyler Grove <tgrove@pingidentity.com>
2020-10-20 15:43:13 +0000

    UIP-3921 + UIP-3923: Astro build process

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/export */
+export { default as AstroWrapper } from './components/AstroWrapper';
+export * from './components/AstroWrapper';
+export { default as Box } from './components/Box';
+export * from './components/Box';
+export { default as Button } from './components/Button';
+export * from './components/Button';
+export { default as Checkbox } from './components/Checkbox';
+export * from './components/Checkbox';
+export { default as CheckboxField } from './components/CheckboxField';
+export * from './components/CheckboxField';
+export { default as Field } from './components/Field';
+export * from './components/Field';
+export { default as Input } from './components/Input';
+export * from './components/Input';
+export { default as Label } from './components/Label';
+export * from './components/Label';
+export { default as Radio } from './components/Radio';
+export * from './components/Radio';
+export { default as RadioField } from './components/RadioField';
+export * from './components/RadioField';
+export { default as RadioGroup } from './components/RadioGroup';
+export * from './components/RadioGroup';
+export { default as Text } from './components/Text';
+export * from './components/Text';
+export { default as TextField } from './components/TextField';
+export * from './components/TextField';
+/* eslint-enable import/export */
