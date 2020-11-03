2c58b53158cf3fb71691e51b47ee89475af64abb
Allegra Weldon <allegraweldon@pingidentity.com>
2020-11-03 20:20:37 +0000

    UIP-4056 Astro Checkbox Field ControlPops

--- packages/astro/src/components/CheckboxField/CheckboxField.stories.js
+++ packages/astro/src/components/CheckboxField/CheckboxField.stories.js
@@ -6,10 +6,19 @@ export default {
   component: CheckboxField,
 };
 
-export const Default = args => (
-  <CheckboxField {...args}>Click me</CheckboxField>
+export const Default = () => (
+  <CheckboxField>Click me</CheckboxField>
 );
 
 export const DefaultSelected = () => (
   <CheckboxField controlProps={{ isDefaultSelected: true }}>Click me</CheckboxField>
 );
+
+export const withControlProps = () => {
+  const [isSelected, setSelected] = React.useState();
+  return (
+    <CheckboxField controlProps={{ onClick: () => setSelected(isSelected) }}>
+      Click me
+    </CheckboxField>
+  );
+};
