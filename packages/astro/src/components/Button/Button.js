97dc0c378999265f824232e80bd0d7de6ccf5488
Elijah Cooper <elijahcooper@pingidentity.com>
2020-12-11 16:40:41 +0000

    UIP-4121: Astro search field

--- packages/astro/src/components/Button/Button.js
+++ packages/astro/src/components/Button/Button.js
@@ -12,6 +12,10 @@ const Button = forwardRef((props, ref) => {
     className,
     isDisabled,
     onPress,
+    onPressStart,
+    onPressEnd,
+    onPressChange,
+    onPressUp,
     ...others
   } = props;
   const buttonRef = useRef();
@@ -40,8 +44,33 @@ const Button = forwardRef((props, ref) => {
 Button.propTypes = {
   /** Whether the button is disabled. */
   isDisabled: PropTypes.bool,
-  /** Handler that is fired when the button is pressed via mouse click or touch event. */
+  /**
+   * Handler that is called when the press is released over the target.
+   * (e: PressEvent) => void
+   */
   onPress: PropTypes.func,
+  /**
+   * Handler that is called when a press interaction starts.
+   * (e: PressEvent) => void
+   */
+  onPressStart: PropTypes.func,
+  /**
+   * Handler that is called when a press interaction ends, either over the target or when the
+   * pointer leaves the target.
+   * (e: PressEvent) => void
+   */
+  onPressEnd: PropTypes.func,
+  /**
+   * Handler that is called when the press state changes.
+   * (isPressed: boolean) => void
+   */
+  onPressChange: PropTypes.func,
+  /**
+   * Handler that is called when a press is released over the target, regardless of whether it
+   * started on the target or not.
+   * (e: PressEvent) => void
+   */
+  onPressUp: PropTypes.func,
   /** The styling variation of the button. */
   variant: PropTypes.oneOf(['default', 'primary', 'text', 'success', 'critical', 'icon', 'inline']),
 };
---
+++ packages/astro/src/styles/forms/search.js
@@ -0,0 +1,23 @@
+export const clearButtonIcon = {
+  color: 'neutral.40',
+};
+
+export const container = {
+  position: 'relative',
+  '> input::-webkit-search-cancel-button, > input::-webkit-search-decoration': {
+    WebkitAppearance: 'none',
+  },
+};
+
+export const icon = {
+  position: 'absolute',
+  top: 8,
+  left: 10,
+  color: 'neutral.40',
+};
+
+export default {
+  clearButtonIcon,
+  container,
+  icon,
+};
