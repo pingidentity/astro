c43fb1675b3d3b93be0b57bb84f9b96a8b65ff5a
Yevhen Kozlov <yevhenkozlov@pingidentity.com>
2020-12-14 23:54:29 +0200

    Astro: better control for Astro Text's story

--- packages/astro/src/components/Text/Text.stories.js
+++ packages/astro/src/components/Text/Text.stories.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Text from './Text';
+import { textVariants } from '../../utils/devUtils/constants/variants.js';
 
 export default {
   title: 'Text',
   component: Text,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: Object.values(textVariants),
+      },
+      defaultValue: Object.values(textVariants)[0],
+    },
+    children: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'Hi, this is some text!',
+    },
+  },
 };
 
-export const Default = args => (
+export const Default = ({ children, ...args }) => (
   <Text {...args}>
-    Hi, this is some text!
+    {children}
   </Text>
 );
