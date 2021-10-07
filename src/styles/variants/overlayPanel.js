0ff7b1194cf98da2a0770752dc2a152bc8970136
Andrew Keohane <andrewkeohane@pingidentity.com>
2021-10-07 19:19:16 +0000

    fix: [UIP-4644] OverlayPanel Improvements

--- src/styles/variants/overlayPanel.js
+++ src/styles/variants/overlayPanel.js
@@ -3,23 +3,40 @@ const overlayPanel = {
   zIndex: 100,
   top: 0,
   bottom: 0,
-  right: 0,
+  right: '-100%',
   background: 'white',
-  display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   borderLeft: '1px solid',
   borderLeftColor: 'neutral.80',
   boxShadow: '-2px 0px 2px 1px rgba(37, 55, 70, 0.15)',
-  minWidth: '50%',
-  maxWidth: '100%',
-  width: '400px',
   p: '25px',
+  transition: 'right 500ms',
+  '&.is-small': {
+    width: '400px',
+  },
+  '&.is-medium': {
+    width: '550px',
+  },
+  '&.is-large': {
+    width: '800px',
+  },
+  '&.is-full': {
+    width: '100%',
+  },
+  '&.is-open': {
+    display: 'flex !important',
+    right: 0,
+  },
 };
 
 const overlayPanelBody = {
+  display: 'none',
   height: '100%',
   width: '100%',
+  '&.is-open': {
+    display: 'flex',
+  },
 };
 
 export default {
