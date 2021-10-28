9f2a54f5e73a3346171c74c47cbbd0632f33d9c7
Andrew Keohane <andrewkeohane@pingidentity.com>
2021-10-28 20:10:15 +0000

    feat: [UIP-4759] Inner Panel Popover

--- src/components/OverlayPanel/OverlayPanel.stories.js
+++ src/components/OverlayPanel/OverlayPanel.stories.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Item } from '@react-stately/collections';
 import { useOverlayPanelState } from '../../hooks';
 import Button from '../Button/Button';
 import OverlayPanel from './OverlayPanel';
-import { OverlayProvider } from '../../index';
-import Box from '../Box';
-import Text from '../Text';
+import { OverlayProvider, Box, Text, List, ListItem, Separator, Messages } from '../../index';
 import { panelSizes } from '../../utils/devUtils/constants/panelSizes';
 
 export default {
@@ -21,6 +20,9 @@ export default {
         options: panelSizes,
       },
       defaultValue: 'medium',
+    },
+    isOpen: {
+      control: 'none',
     },
   },
   parameters: {
@@ -48,7 +50,7 @@ export const Default = ({ ...args }) => {
       </Text>
       <br />
       <Button onPress={state.open}>Open Panel</Button>
-      <OverlayPanel {...args} isOpen={state.isOpen} >
+      <OverlayPanel isOpen={state.isOpen} {...args}>
         <Box>
           <Button
             onPress={state.close}
@@ -61,5 +63,106 @@ export const Default = ({ ...args }) => {
         </Box>
       </OverlayPanel>
     </OverlayProvider>
+  );
+};
+
+export const InnerPanel = ({ ...args }) => {
+  const state = useOverlayPanelState();
+  const innerState = useOverlayPanelState();
+
+  const [messagesOpen, setMessagesOpen] = useState(false);
+
+  const toggleMessagesOpen = () => {
+    setMessagesOpen(!messagesOpen);
+  };
+
+  const closeOuterPanel = () => {
+    if (innerState.isOpen) {
+      innerState.close();
+    }
+    state.close();
+  };
+
+  const inner = (
+    <>
+      {
+        innerState.isOpen &&
+        <OverlayPanel
+          variant="overlayPanel.overlayPanelInner" // applies higher z-index
+          isOpen={innerState.isOpen}
+          {...args}
+        >
+          <Box>
+            <Button onPress={innerState.close}>Close Inner Panel</Button>
+            <Text pt="md">
+              Children render here.
+            </Text>
+          </Box>
+        </OverlayPanel>
+      }
+    </>
+  );
+
+  const outer = (
+    // should have higher z-index applied
+    <>
+      {
+        state.isOpen &&
+        <OverlayPanel isOpen={state.isOpen} sx={{ p: '0px' }} {...args}>
+          <Box sx={{ p: '12px' }}>
+            <Button onPress={closeOuterPanel}>Close Panel</Button>
+            <Text pt="md" mb="24px">
+              Children render here.
+            </Text>
+            <List>
+              <ListItem>
+                <Text variant="itemTitle" alignSelf="center" mr="auto">Form 1</Text>
+              </ListItem>
+              <Separator margin="0" />
+              <ListItem title="Form 2">
+                <Text variant="itemTitle" alignSelf="center" mr="auto">Form 2</Text>
+              </ListItem>
+              <Separator margin="0" />
+              <ListItem title="Form 3">
+                <Text variant="itemTitle" alignSelf="center" mr="auto">Form 3</Text>
+              </ListItem>
+              <Separator margin="0" />
+              <ListItem title="Form 4">
+                <Text variant="itemTitle" alignSelf="center" mr="auto">Form 4</Text>
+              </ListItem>
+              <Separator margin="0" />
+              <ListItem title="Form 5">
+                <Text variant="itemTitle" alignSelf="center" mr="auto">Form 5</Text>
+              </ListItem>
+              <Separator margin="0" />
+            </List>
+            <br />
+            <Button onPress={toggleMessagesOpen}>Toggle Messages</Button>
+            <br />
+            <Button onPress={innerState.open}>Open Inner Panel</Button>
+
+
+            {inner}
+
+          </Box>
+        </OverlayPanel>
+      }
+    </>
+  );
+
+  return (
+    // Application must be wrapped in an OverlayProvider so that it can be hidden from screen
+    // readers when an overlay opens.
+    <>
+      <OverlayProvider>
+        <Button onPress={state.open}>Open Panel</Button>
+        {outer}
+      </OverlayProvider>
+      { messagesOpen &&
+        <Messages sx={{ zIndex: 3 }} >
+          <Item key="message2" status="success">Z Index higher than inner pannel</Item>
+        </Messages>
+      }
+    </>
   );
 };
