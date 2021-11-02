c32f06b5d6f099825291afdc066643df8719fac4
Eli Cooper <elijahcooper@pingidentity.com>
2021-11-02 11:46:46 -0500

    fix: Storybook source code not displaying for many components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,10 @@
-module.exports = _dirname => ({
-	stories: ['../src/**/*.@(story|stories).@(js|jsx|mdx)'],
-	addons: [
-		'@storybook/addon-actions/register',
-		'@storybook/addon-links',
-		'@storybook/addon-a11y/register',
-		'@storybook/addon-viewport/register',
-		'@storybook/addon-docs',
-		'@storybook/addon-storysource',
-	],
-	webpackFinal: async (config) => {
-		const rules = config.module.rules;
-
-		// modify storybook's file-loader rule to avoid conflicts with svgr
-		const fileLoaderRule = rules.find(rule => (rule.test && rule.test.test && rule.test.test('.svg')));
-		fileLoaderRule.exclude = /\@mdi|icons\//;
-
-
-
-		rules.push({
-			test: /\.story.jsx$/,
-			loaders: [require.resolve('@storybook/source-loader')],
-			enforce: 'pre',
-		});
-
-		rules.push({
-			test: /\.svg$/,
-			loaders: ['@svgr/webpack'],
-		});
-
-		return config;
-	},
-});
+module.exports = {
+  stories: [
+    "../@(src|stories)/**/*.@(story|stories).@(js|jsx|mdx)"
+  ],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+}
