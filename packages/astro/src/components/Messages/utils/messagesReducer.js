09364862f75d9e8d59fee6b4a5e651380f3a2175
Mariia Yevdokymova <mariiayevdokymova@pingidentity.com>
2021-11-29 18:37:12 +0000

    feat: [UIP-4838] Auto removing messages

--- packages/astro/src/components/Messages/utils/messagesReducer.js
+++ packages/astro/src/components/Messages/utils/messagesReducer.js
@@ -5,6 +5,7 @@ import { v4 as uuid } from 'uuid';
 */
 export const messagesActions = {
   ADD_MESSAGE: 'ADD_MESSAGE',
+  HIDE_MESSAGE: 'HIDE_MESSAGE',
   REMOVE_MESSAGE: 'REMOVE_MESSAGE',
   CLEAR_MESSAGES: 'CLEAR_MESSAGES',
 };
@@ -19,6 +20,14 @@ export const addMessage = message => ({
 });
 
 /**
+ * Create an action to hide a message by key
+ */
+export const hideMessage = key => ({
+  type: messagesActions.HIDE_MESSAGE,
+  key,
+});
+
+/**
  * Create an action to remove a message by key
  */
 export const removeMessage = key => ({
@@ -30,12 +39,16 @@ export const removeMessage = key => ({
  * Create an action to add a message and then remove it if there's a timeout
  */
 export const showMessage = (messageArg, timeout = -1) => (dispatch) => {
-  const message = { id: uuid(), ...messageArg };
+  const message = { key: uuid(), ...messageArg };
   dispatch(addMessage(message));
 
   if (timeout >= 0) {
     setTimeout(() => {
-      dispatch(removeMessage(message));
+      dispatch(hideMessage(message.key));
+
+      setTimeout(() => {
+        dispatch(removeMessage(message.key));
+      }, 200);
     }, timeout);
   }
 
@@ -50,7 +63,17 @@ const messagesReducer = (
   state = [], { type, message, key } = {},
 ) => {
   switch (type) {
-    case messagesActions.ADD_MESSAGE: return [...state, { key: uuid(), ...message }];
+    case messagesActions.ADD_MESSAGE: return [...state, { ...message }];
+    case messagesActions.HIDE_MESSAGE:
+      return state.map((search) => {
+        if (search.key === key) {
+          return {
+            ...search,
+            isHidden: true,
+          };
+        }
+        return search;
+      });
     case messagesActions.REMOVE_MESSAGE:
       return state.filter(search => key !== search.key);
     case messagesActions.CLEAR_MESSAGES: return [];
@@ -60,6 +83,7 @@ const messagesReducer = (
 
 messagesReducer.actions = {
   addMessage,
+  hideMessage,
   removeMessage,
   showMessage,
   clearMessages,
