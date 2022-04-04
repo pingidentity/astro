3f3ada3643ab9760a7308b3b73b92b2a2cfcd8f8
Allegra Weldon <allegraweldon@pingidentity.com>
2022-04-04 21:21:20 +0000

    feat: [UIP-5028] UI Library Global Styles Override

--- packages/astro/src/components/AstroWrapper/AstroWrapper.js
+++ packages/astro/src/components/AstroWrapper/AstroWrapper.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui';
+import React, { forwardRef, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider, merge } from 'theme-ui';
 import { Global, css } from '@emotion/react';
 import emotionNormalize from 'emotion-normalize';
 import Box from '../Box';
-import theme from '../../styles/theme';
+import astroTheme from '../../styles/theme';
 
 export const GlobalStyles = () => (
   <Global
@@ -33,56 +34,119 @@ export const GlobalStyles = () => (
 
 /**
  * The main wrapper for the Astro application.
- * It provides the standard background, some global styles, and the Astro theme.
+ * The AstroWrapper provides a standard background,
+ * some global styles, and the Astro theme by default.
+ *
+ * _Note: For UI Library and Astro CSS conflicts, we supply a theme override located at_
+ * `@pingux/astro/lib/styles/themes/overrideUILib`
  */
-const AstroWrapper = ({ children, ...props }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Box bg="background" height="100%" {...props}>
-      {children}
-    </Box>
-  </ThemeProvider>
-);
+const AstroWrapper = forwardRef((props, ref) => {
+  const {
+    defaultTheme,
+    themeOverrides,
+    children,
+    ...others
+  } = props;
+
+  // Unfortunately because this is adding styles, we cannot write a proper test for this.
+  /* istanbul ignore next */
+  const theme = useMemo(() => merge(defaultTheme, ...themeOverrides),
+    [defaultTheme, themeOverrides]);
+
+  return (
+    <ThemeProvider ref={ref} theme={theme} {...others}>
+      <GlobalStyles />
+      <Box bg="background" height="100%" {...props}>
+        {children}
+      </Box>
+    </ThemeProvider>
+  );
+});
+
+AstroWrapper.propTypes = {
+  /** Array of theme objects which will be merged with the default theme.
+     * In the case of clashes, these will take priority.
+     * Useful for customizing the default variants, adding new ones,
+     *  or overriding other theme values. */
+  themeOverrides: PropTypes.arrayOf(PropTypes.shape({})),
+  /** The default theme applied to the Astro components.
+   * Overriding this is an advanced use case so
+   * please understand potential reprecussions before editing */
+  defaultTheme: PropTypes.shape({}),
+};
+
+AstroWrapper.defaultProps = {
+  defaultTheme: astroTheme,
+  themeOverrides: [{}],
+};
 
 /**
  * Wrapper for the Astro application w/o global styles.
  * It provides the standard background and the Astro theme.
  */
-export const PageWrapper = ({ children, ...props }) => (
-  <ThemeProvider theme={theme}>
-    <Global
-      styles={
-        css`
-          @import url("https://use.typekit.net/icz8cni.css");
+export const PageWrapper = forwardRef((props, ref) => {
+  const {
+    defaultTheme,
+    themeOverrides,
+    children,
+    ...others
+  } = props;
 
-          [data-tippy-root] {
-            max-width: calc(100vw - 10px);
-          }
-        `
+  const theme = useMemo(() => merge(defaultTheme, ...themeOverrides),
+    [defaultTheme, themeOverrides]);
+  return (
+    <ThemeProvider ref={ref} theme={theme} {...others}>
+      <Global
+        styles={
+  css`
+    @import url("https://use.typekit.net/icz8cni.css");
+
+    [data-tippy-root] {
+      max-width: calc(100vw - 10px);
+    }
+  `
+}
+      />
+      <Box
+        css={
+  css`
+    ${emotionNormalize}
+
+    * {
+      box-sizing: border-box;
+      font-family: "Helvetica Neue", Helvetica, sans-serif;
+
+      .is-disabled {
+        opacity: 0.5;
+        pointer-events: none;
       }
-    />
-    <Box
-      css={
-        css`
-          ${emotionNormalize}
+    }
+  `
+}
+        {...props}
+      >
+        {children}
+      </Box>
+    </ThemeProvider>
+  );
+});
 
-          * {
-            box-sizing: border-box;
-            font-family: "Helvetica Neue", Helvetica, sans-serif;
+PageWrapper.propTypes = {
+  /** Array of theme objects which will be merged with the default theme.
+     * In the case of clashes, these will take priority.
+     * Useful for customizing the default variants, adding new ones,
+     *  or overriding other theme values. */
+  themeOverrides: PropTypes.arrayOf(PropTypes.shape({})),
+  /** The default theme applied to the Astro components.
+   * Overriding this is an advanced use case so
+   * please understand potential reprecussions before editing */
+  defaultTheme: PropTypes.shape({}),
+};
 
-            .is-disabled {
-              opacity: 0.5;
-              pointer-events: none;
-            }
-          }
-        `
-      }
-      {...props}
-    >
-      {children}
-    </Box>
-  </ThemeProvider>
-);
+PageWrapper.defaultProps = {
+  defaultTheme: astroTheme,
+  themeOverrides: [{}],
+};
 
 export { ThemeProvider };
 export default AstroWrapper;
