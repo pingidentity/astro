29f5be269fbf94f12c10c44bdb110aa89de24f46
Andrew Keohane <andrewkeohane@pingidentity.com>
2022-04-08 18:55:31 +0000

    fix: [UIP-5338] ComboBox Improvements

--- src/components/TimeZonePicker/TimeZonePicker.js
+++ src/components/TimeZonePicker/TimeZonePicker.js
@@ -104,12 +104,12 @@ const TimeZonePicker = forwardRef((props, ref) => {
     return aNum - bNum;
   };
 
-  const items = useMemo(() => {
-    if (filteredTimezones.length === 0) {
-      return <Item key={emptySearchText}>{emptySearchText}</Item>;
-    }
+  const checkIsSelectedItem = () => {
+    return timeZones.filter(tz => tz.timeZone === search).length > 0;
+  };
 
-    return filteredTimezones.sort(sortByGMT).map(({ gmt, time, timeZone }) => (
+  const renderTimeZones = (timeZonesToRender) => {
+    return timeZonesToRender.sort(sortByGMT).map(({ gmt, time, timeZone }) => (
       <Item key={timeZone} data-id={timeZone} textValue={timeZone}>
         <Box flexDirection="row" justifyContent="space-between" width="100%">
           <Box flexDirection="row">
@@ -122,7 +122,14 @@ const TimeZonePicker = forwardRef((props, ref) => {
         </Box>
       </Item>
     ));
-  }, [emptySearchText, filteredTimezones]);
+  };
+
+  const items = useMemo(() => {
+    if (filteredTimezones.length === 0) {
+      return <Item key={emptySearchText}>{emptySearchText}</Item>;
+    }
+    return renderTimeZones(checkIsSelectedItem() ? timeZones : filteredTimezones);
+  }, [emptySearchText, filteredTimezones, search, timeZones]);
 
   const comboBoxFieldProps = useMemo(
     () => ({
@@ -138,7 +145,7 @@ const TimeZonePicker = forwardRef((props, ref) => {
   );
 
   return (
-    <ComboBoxField {...comboBoxFieldProps} disabledKeys={[emptySearchText]}>
+    <ComboBoxField {...comboBoxFieldProps} disabledKeys={[emptySearchText]} menuTrigger="focus" >
       {items}
     </ComboBoxField>
   );
