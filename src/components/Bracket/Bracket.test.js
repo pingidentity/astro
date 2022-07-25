8f382711d3f1b492f4fb0a05735a3e8cc9162a6d
Andrew Keohane <andrewkeohane@pingidentity.com>
2022-07-25 16:35:23 +0000

    feat: [UIP-5493] Bracket component

---
+++ src/components/Bracket/Bracket.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Bracket from '.';
+import axeTest from '../../utils/testUtils/testAxe';
+
+const testId = 'test-bracket';
+const defaultProps = {
+  'data-testid': testId,
+};
+const getComponent = (props = {}) => render(
+  <Bracket {...defaultProps} {...props} />,
+);
+
+// // Need to be added to each test file to test accessibility using axe.
+axeTest(getComponent);
+
+
+test('default bracket', () => {
+  getComponent();
+  const bracket = screen.getByTestId(testId);
+  expect(bracket).toBeInTheDocument();
+});
+
+test('additional svg does not render if isLast=true', () => {
+  getComponent({ isLast: true });
+  expect(screen.queryByTestId('isLastLayer')).not.toBeInTheDocument();
+});
+
+test('additional svg does render if isLast=false', () => {
+  getComponent({ isLast: false });
+  expect(screen.queryByTestId('isLastLayer')).toBeInTheDocument();
+});
