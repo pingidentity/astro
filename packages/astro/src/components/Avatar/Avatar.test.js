61e0749e1de5dfc271f895433e5d139b39734105
Illia Lukashchuk <illialukashchuk@pingidentity.com>
2022-08-10 17:12:48 +0000

    feat: [UIP-5433] add accessible list and panel

--- packages/astro/src/components/Avatar/Avatar.test.js
+++ packages/astro/src/components/Avatar/Avatar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import faker from 'faker';
+import { faker } from '@faker-js/faker';
 import axeTest from '../../utils/testUtils/testAxe';
 import { render, screen } from '../../utils/testUtils/testWrapper';
 import Avatar from '.';
