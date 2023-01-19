ef19147a63cdce92c08b572cc4d01b1ec504d81d
Jeremy Bissonnette <jeremybissonnette@pingidentity.com>
2023-01-19 16:39:32 +0000

    chore: rename bulletin to callout

--- packages/astro/src/styles/variants/variants.js
+++ packages/astro/src/styles/variants/variants.js
@@ -1,15 +1,18 @@
 import accordion from './accordion';
 import boxes from './boxes';
+import callout from './callout';
 import codeView from './codeView';
-import images from './images';
+import collapsiblePanel from './collapsiblePanel';
+import dataTable from './../../components/DataTable/DataTable.styles';
 import imageUpload from './imageUpload';
+import images from './images';
 import links from './links';
 import listBox from './listBox';
 import loader from './loader';
-import modal from './modal';
 import menu from './menu';
 import menuItem from './menuItem';
 import messages from './messages';
+import modal from './modal';
 import navBar from './navBar';
 import numberField from './numberField';
 import overlayPanel from './overlayPanel';
@@ -20,25 +23,24 @@ import stepper from './stepper';
 import table from './table';
 import * as tabs from './tabs';
 import tooltip from './tooltip';
-import collapsiblePanel from './collapsiblePanel';
-import bulletin from './bulletin';
-import dataTable from './../../components/DataTable/DataTable.styles';
 
 export default {
+  ...tabs,
   accordion,
   boxes,
-  bulletin,
+  callout,
   codeView,
-  images,
+  collapsiblePanel,
+  dataTable,
   imageUpload,
-  loader,
+  images,
   links,
   listBox,
+  loader,
   menu,
   menuItem,
   messages,
   modal,
-  collapsiblePanel,
   navBar,
   numberField,
   overlayPanel,
@@ -47,7 +49,5 @@ export default {
   separator,
   stepper,
   table,
-  ...tabs,
   tooltip,
-  dataTable,
 };
