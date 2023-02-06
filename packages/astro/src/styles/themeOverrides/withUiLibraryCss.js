aed79668ca9db752d648f19dd4787a12a9875746
Volodymyr Svirhun <volodymyrsvirhun@pingidentity.com>
2023-02-06 16:30:26 +0000

    feat: [UIP-5983] setup chromatic only stories

---
+++ packages/astro/src/styles/themeOverrides/withUiLibraryCss.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { ThemeProvider } from 'theme-ui';
+
+import uiLibraryOverride from './uiLibraryOverride';
+
+const UI_LIBRARY_CSS_LINK = 'https://assets.pingone.com/ux/ui-library/5.0.2/css/ui-library.css';
+
+const WithUiLibraryCss = (Story) => {
+  useEffect(() => {
+    const uiLibraryCss = document.createElement('link');
+    uiLibraryCss.rel = 'stylesheet';
+    uiLibraryCss.className = 'uiLibraryCssLink';
+    uiLibraryCss.href = UI_LIBRARY_CSS_LINK;
+    document.getElementsByTagName('head')[0].appendChild(uiLibraryCss);
+
+    return () => document.querySelectorAll('.uiLibraryCssLink').forEach((link) => {
+      link.remove();
+    });
+  }, []);
+
+  return (
+    <ThemeProvider theme={uiLibraryOverride}>
+      <Story />
+    </ThemeProvider>
+  );
+};
+
+export default WithUiLibraryCss;
