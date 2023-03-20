679b01378d4bbf9482f8531ff48b34611f8226f5
Allegra Weldon <allegraweldon@pingidentity.com>
2023-03-20 17:44:21 +0000

    fix: [UIP-6194] Update PageHeader Recipe to match mock changes
    ## Describe your changes
    With Page Header component being deprecated, the warning says to replace the component with the code in the Page Header recipe, however that code is outdated and different from designs. The purpose of this MR is to ensure the page header recipe is in alignment with the current figma mock (not the future one).
    https://www.figma.com/file/bpbEw54gTTcdIiZs4PKK1p/Astro-Specs?node-id=3408-15250&t=hVyBd95FjKiM9aLt-0
    
    ## Any testing tips
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [NA] I have added tests for any new or fixed functionality to prevent regressions
    - [NA] I have added clarifying code comments to blocks which other devs may have questions on
    
    Closes UIP-6194

--- src/recipes/PageHeader.stories.js
+++ src/recipes/PageHeader.stories.js
@@ -1,63 +1,33 @@
 import React from 'react';
 import PlusIcon from 'mdi-react/PlusIcon';
-import { Box, Button, IconButton, Icon, Text } from '../index';
+import { Box, Link, IconButton, Icon, Text } from '../index';
 
 export default {
   title: 'Recipes/Page Header',
 };
 export const Default = () => {
-  const heading = 'Title of the Page';
-  const description = 'The description of the page. The description of the page. The description of the page. The description of the page. The description of the page. The description of the page. The description of the page. The description of the page. The description of the page.';
-
   return (
-    <Box >
-      <Box>
-        <Box
-          align="center"
-          isRow
-          mb="xs"
-          role="heading"
-          aria-level="1"
-        >
-          <Text variant="title" fontWeight={3}>
-            {heading}
-          </Text>
-          <Button variant="inlinePrimary" ml="sm" >
-            <Icon
-              icon={PlusIcon}
-              color="white"
-              size={13}
-              mr="4px"
-            />
-            {'\u00a0'}
-            Add
-          </Button>
-        </Box>
-        <Text variant="bodyWeak">
-          {description}
+    <>
+      <Box
+        align="center"
+        isRow
+        mb="xs"
+        role="heading"
+        aria-level="1"
+      >
+        <Text fontSize="xx" fontWeight="3" fontColor="text.primary">
+          Title of the Page
         </Text>
+        <IconButton aria-label="icon button" ml="sm" variant="inverted">
+          <Icon icon={PlusIcon} size="sm" />
+        </IconButton>
       </Box>
-
-      <Box mt="xl">
-        <Box
-          align="center"
-          isRow
-          mb="xs"
-          role="heading"
-          aria-level="1"
-        >
-          <Text variant="title" fontWeight={3}>
-            {heading}
-          </Text>
-          <IconButton aria-label="icon button" ml="sm" mt="3px" variant="inverted" >
-            <Icon icon={PlusIcon} color="white" size="xs" />
-          </IconButton>
-        </Box>
-        <Text variant="bodyWeak">
-          {description}
-        </Text>
-      </Box>
-    </Box>
-
+      <Text fontSize="sm" color="text.secondary" fontWeight="0" width="800px">
+        The description of the page. The description of the page. The description of the page.
+        The description of the page.The description of the page. The description of the page.
+        The description of the page. The description of the page. The description of the page.&nbsp;
+        <Link href="https://uilibrary.ping-eng.com/" sx={{ fontSize: 'sm' }}>Learn more</Link>
+      </Text>
+    </>
   );
 };
