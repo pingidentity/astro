621556118067ad8191672da13e66791850c5f4cb
Jeremy Bissonnette <jeremybissonnette@pingidentity.com>
2023-04-13 16:21:02 +0000

    fix: [UIP-6041]: LinkedListView: incorrect hover state
    ## Describe your changes
    - Refactor Staging and Production to share one component ListElement, so that hover state management can be handled in one place
    - implement use of sx object
    - fix hover state to match ListAndPanel, similar to https://gitlab.corp.pingidentity.com/ux/pingux/-/merge_requests/2070
    - refactor hover state logic into hook: useOverlappingMenuHoverState
    
    ## Any testing tips
    see comment about hover state limitations in [ListAndPanel hover state MR](https://gitlab.corp.pingidentity.com/ux/pingux/-/merge_requests/2070) 
    
    ## Checklist before requesting a review
    - [:white_check_mark: ] I have performed a self-review of my code
    - [:white_check_mark: ] I have added tests for any new or fixed functionality to prevent regressions
    - [:white_check_mark: ] I have added clarifying code comments to blocks which other devs may have questions on

---
+++ src/hooks/useOverlappingMenuHoverState/useOverlappingMenuHoverState.test.js
@@ -0,0 +1,151 @@
+import React, { useRef } from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import {
+  IconButton,
+  ListItem,
+  Menu,
+  PopoverMenu,
+} from '../../index';
+
+import useOverlappingMenuHoverState from './useOverlappingMenuHoverState';
+
+const IS_HOVERED = 'isHovered';
+
+const TestComponent = () => {
+  const listItemRef = useRef();
+
+  const {
+    handleHoverEnd,
+    handleHoverStart,
+    handleMenuHoverEnd,
+    handleMouseMove,
+    isHovered,
+  } = useOverlappingMenuHoverState({ listItemRef });
+
+  return (
+    <>
+      <ListItem
+        isHovered={isHovered}
+        onHoverEnd={handleHoverEnd}
+        onHoverStart={handleHoverStart}
+        onMouseMove={handleMouseMove}
+        role="listitem"
+        ref={listItemRef}
+      >
+        <PopoverMenu>
+          <IconButton />
+          <Menu
+            onAction={handleHoverEnd}
+            onHoverEnd={handleMenuHoverEnd}
+            onHoverStart={handleHoverStart}
+          />
+        </PopoverMenu>
+      </ListItem>
+      {isHovered && IS_HOVERED}
+    </>
+  );
+};
+
+const getComponent = () => render(<TestComponent />);
+
+describe('useOverlappingMenuHoverState', () => {
+  describe('when the menu is closed', () => {
+    describe('when the ListItem has not been hovered', () => {
+      it('it should not be hovered', () => {
+        getComponent();
+
+        expect(screen.queryByText(IS_HOVERED)).not.toBeInTheDocument();
+      });
+    });
+
+    describe('when the ListItem is hovered', () => {
+      it('it should be hovered', async () => {
+        getComponent();
+
+        await userEvent.hover(screen.getByRole('listitem'));
+
+        screen.getByText(IS_HOVERED);
+      });
+    });
+  });
+
+  describe('when the menu is opened', () => {
+    describe('when the ListItem is hovered', () => {
+      it('it should be hovered', async () => {
+        getComponent();
+
+        await userEvent.click(screen.getByRole('button'));
+
+        screen.getByText(IS_HOVERED);
+      });
+    });
+
+    describe('when the ListItem is unhovered', () => {
+      it('it should not be hovered', async () => {
+        getComponent();
+
+        await userEvent.click(screen.getByRole('button'));
+
+        await userEvent.unhover(screen.getByRole('listitem'));
+
+        expect(screen.queryByText(IS_HOVERED)).not.toBeInTheDocument();
+      });
+    });
+
+    describe('when the Menu is hovered', () => {
+      it('it should be hovered', async () => {
+        getComponent();
+
+        await userEvent.click(screen.getByRole('button'));
+
+        await userEvent.hover(screen.getByRole('menu'));
+
+        screen.getByText(IS_HOVERED);
+      });
+    });
+
+    describe('when hover moves from the menu to the ListItem', () => {
+      // FIXME: upgrade RTL to v14 (UIP-6248), then fix this test,
+      // then remove related coveragePathIgnorePatterns in jest.config.js
+      it('it should be hovered', async () => {
+        getComponent();
+
+        await userEvent.click(screen.getByRole('button'));
+
+        await userEvent.hover(screen.getByRole('menu'));
+        await userEvent.hover(screen.getByRole('listitem'));
+
+        screen.getByText(IS_HOVERED);
+      });
+    });
+
+    describe('when hover moves from the menu to unhovered', () => {
+      it('it should not be hovered', async () => {
+        getComponent();
+
+        await userEvent.click(screen.getByRole('button'));
+
+        await userEvent.hover(screen.getByRole('menu'));
+        await userEvent.unhover(screen.getByRole('menu'));
+
+        expect(screen.queryByText(IS_HOVERED)).not.toBeInTheDocument();
+      });
+    });
+
+    describe('when hover moves from the menu to ListItem to unhoverd', () => {
+      it('it should not be hovered', async () => {
+        getComponent();
+
+        await userEvent.click(screen.getByRole('button'));
+
+        await userEvent.hover(screen.getByRole('menu'));
+        await userEvent.hover(screen.getByRole('listitem'));
+        await userEvent.unhover(screen.getByRole('listitem'));
+
+        expect(screen.queryByText(IS_HOVERED)).not.toBeInTheDocument();
+      });
+    });
+  });
+});
--- src/recipes/LinkedListView.stories.js
+++ src/recipes/LinkedListView.stories.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import DotsVerticalIcon from 'mdi-react/DotsVerticalIcon';
 
+import useOverlappingMenuHoverState from '../hooks/useOverlappingMenuHoverState';
 import {
   Badge,
   Box,
   Icon,
   IconButton,
   Item,
+  ListItem,
   ListView,
   Menu,
-  OverlayProvider,
   PopoverMenu,
   Separator,
   Text,
@@ -48,7 +49,6 @@ const testData = [
     hasSeparator: false,
     hasInsetSeparator: true,
     badgeText: 'default',
-    expirationText: 'Expiring On: 2022-12-08',
     childrenObjects: [
       {
         key: 'Staging Policy',
@@ -115,6 +115,93 @@ const testSort = array => {
 const unsorted = flattenArray(testData);
 const sorted = testSort(unsorted);
 
+const sx = {
+  badge: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    ml: 'lg',
+    p: '3px 5px 2px 5px',
+  },
+  bottomBracket: {
+    container: {
+      ml: 12,
+      top: -2,
+      position: 'relative',
+      maxWidth: 15,
+    },
+    separator: {
+      backgroundColor: 'decorative.7',
+      marginLeft: '2px !important',
+      maxHeight: '30px',
+      width: '2px !important',
+    },
+    separatorBox: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      minWidth: 9,
+    },
+  },
+  expirationBadge: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    border: 'solid 1px',
+    borderColor: 'neutral.80',
+    p: '3px 5px 2px 5px',
+  },
+  listElement: {
+    container: {
+      height: 73,
+      pl: 24,
+      pr: 14,
+    },
+    subText: {
+      color: 'text.secondary',
+      fontSize: 'sm',
+      fontWeight: 0,
+    },
+    text: {
+      color: 'text.primary',
+      fontSize: 'md',
+      fontWeight: 1,
+    },
+    textBox: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginRight: 'auto',
+    },
+  },
+  listViewItem: {
+    minHeight: '75px',
+    padding: 1,
+    '&.has-inset-separator': {
+      '&:before': {
+        borderBottom: '1px solid',
+        borderBottomColor: 'line.light',
+        bottom: 0,
+        content: '""',
+        position: 'absolute',
+        right: 0,
+        width: 'calc(100% - 43px)',
+      },
+    },
+    '&.is-focused': {
+      '&:after': {
+        borderBottomColor: 'focus',
+        bottom: 0,
+        content: '""',
+        position: 'absolute',
+        right: 0,
+        width: 'calc(100% - 43px)',
+      },
+    },
+  },
+  topBracket: {
+    top: 50,
+    left: 12,
+    bottom: 0,
+    position: 'absolute',
+  },
+};
 export const Default = ({ ...args }) => {
   const [theseItems] = useState([...sorted]);
 
@@ -122,8 +209,8 @@ export const Default = ({ ...args }) => {
   const BottomBracket = () => {
     const color = '#CACED3';
     return (
-      <Box sx={{ width: '15px', position: 'relative' }}>
-        <Box flexBasis="50%">
+      <Box sx={sx.bottomBracket.container}>
+        <Box flexBasis="53%">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             version="1.1"
@@ -163,11 +250,35 @@ export const Default = ({ ...args }) => {
     );
   };
 
+  // the vector shield and key icon.
+  const ShieldIcon = props => (
+    <svg width="19" height="23" viewBox="0 0 19 23" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M9.5 7.33342C9.77627 7.33342 10.0412 7.44316 10.2366 7.63851C10.4319 7.83386 10.5417 8.09881 10.5417 8.37508C10.5417 8.65135 10.4319 8.9163 10.2366 9.11165C10.0412 9.307 9.77627 9.41675 9.5 9.41675C9.22373 9.41675 8.95878 9.307 8.76343 9.11165C8.56808 8.9163 8.45833 8.65135 8.45833 8.37508C8.45833 8.09881 8.56808 7.83386 8.76343 7.63851C8.95878 7.44316 9.22373 7.33342 9.5 7.33342ZM18.875 10.4584C18.875 16.2397 14.875 21.6459 9.5 22.9584C4.125 21.6459 0.125 16.2397 0.125 10.4584V4.20842L9.5 0.041748L18.875 4.20842V10.4584ZM9.5 5.25008C8.6712 5.25008 7.87634 5.57932 7.29029 6.16537C6.70424 6.75142 6.375 7.54628 6.375 8.37508C6.375 9.73967 7.23958 10.8959 8.45833 11.323V17.7501H10.5417V15.6667H12.625V13.5834H10.5417V11.323C11.7604 10.8959 12.625 9.73967 12.625 8.37508C12.625 7.54628 12.2958 6.75142 11.7097 6.16537C11.1237 5.57932 10.3288 5.25008 9.5 5.25008Z" />
+    </svg>
+  );
+
+  const Shield = ({ isParent }) => (
+    <Box
+      alignItems="center"
+      justifyContent="center"
+      minWidth={24}
+      sx={{ position: 'relative' }}
+    >
+      <Icon
+        icon={ShieldIcon}
+        color={isParent ? 'decorative.7' : 'accent.40'}
+        size={24}
+        isLinked={isParent}
+      />
+      {isParent && <TopBracket />}
+    </Box>
+  );
+
   // the top portion of the bracket svg, this is basically just a straight vertical line.
   const TopBracket = () => {
     const color = '#CACED3';
     return (
-      <Box width={15} sx={{ position: 'absolute', bottom: 0, left: 12, top: 50 }}>
+      <Box width={15} sx={sx.topBracket}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
@@ -195,238 +306,162 @@ export const Default = ({ ...args }) => {
     );
   };
 
-  // the vector shield and key icon.
-  const ShieldVector = props => {
-    return (
-      <svg width="19" height="23" viewBox="0 0 19 23" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path d="M9.5 7.33342C9.77627 7.33342 10.0412 7.44316 10.2366 7.63851C10.4319 7.83386 10.5417 8.09881 10.5417 8.37508C10.5417 8.65135 10.4319 8.9163 10.2366 9.11165C10.0412 9.307 9.77627 9.41675 9.5 9.41675C9.22373 9.41675 8.95878 9.307 8.76343 9.11165C8.56808 8.9163 8.45833 8.65135 8.45833 8.37508C8.45833 8.09881 8.56808 7.83386 8.76343 7.63851C8.95878 7.44316 9.22373 7.33342 9.5 7.33342ZM18.875 10.4584C18.875 16.2397 14.875 21.6459 9.5 22.9584C4.125 21.6459 0.125 16.2397 0.125 10.4584V4.20842L9.5 0.041748L18.875 4.20842V10.4584ZM9.5 5.25008C8.6712 5.25008 7.87634 5.57932 7.29029 6.16537C6.70424 6.75142 6.375 7.54628 6.375 8.37508C6.375 9.73967 7.23958 10.8959 8.45833 11.323V17.7501H10.5417V15.6667H12.625V13.5834H10.5417V11.323C11.7604 10.8959 12.625 9.73967 12.625 8.37508C12.625 7.54628 12.2958 6.75142 11.7097 6.16537C11.1237 5.57932 10.3288 5.25008 9.5 5.25008Z" />
-      </svg>
-    );
-  };
-
   // reusable piece of code that handles the iconbutton and popover on the right side of the rows
-  const IconWithPopover = () => {
-    return (
-      <PopoverMenu direction="left">
-        <Box alignItems="center" alignSelf="center">
-          <IconButton aria-label="Menu Button">
-            <Icon icon={DotsVerticalIcon} size="xs" color="Neutral.40" m="0.61px" />
-          </IconButton>
-        </Box>
-        <Menu
-          direction="left"
-          sx={{
-            minWidth: '155px',
-            minHeight: '144px',
-          }}
-        >
-          <Item key="view">View</Item>
-          <Item key="edit">Edit</Item>
-          <Item key="rename">Rename</Item>
-          <Item key="delete" textValue="delete">
-            <Text color="critical.bright">Delete</Text>
-          </Item>
-        </Menu>
-      </PopoverMenu>
-    );
-  };
+  const IconWithPopover = ({
+    handleHoverEnd,
+    handleHoverStart,
+    handleMenuHoverEnd,
+  }) => (
+    <PopoverMenu direction="left">
+      <Box alignItems="center" alignSelf="center">
+        <IconButton aria-label="Menu Button">
+          <Icon icon={DotsVerticalIcon} size="xs" color="Neutral.40" m="0.61px" />
+        </IconButton>
+      </Box>
+      <Menu
+        direction="left"
+        onAction={handleHoverEnd}
+        onHoverEnd={handleMenuHoverEnd}
+        onHoverStart={handleHoverStart}
+        sx={{ minWidth: '155px', minHeight: '144px' }}
+      >
+        <Item key="view">View</Item>
+        <Item key="edit">Edit</Item>
+        <Item key="rename">Rename</Item>
+        <Item key="delete" textValue="delete">
+          <Text color="critical.bright">Delete</Text>
+        </Item>
+      </Menu>
+    </PopoverMenu>
+  );
 
   // made these separate components because staging and production both use them.
-  const DefaultBadge = () => {
-    return (
+  const DefaultBadge = () => (
+    <Badge
+      label="Default"
+      bg="active"
+      sx={sx.badge}
+    />
+  );
+
+  const ExpirationBadge = ({
+    expirationText,
+    badgeText,
+  }) => (
+    <Box
+      sx={!badgeText && { paddingLeft: '72px' }}
+      flexGrow={1}
+      alignItems="center"
+      alignSelf="center"
+    >
       <Badge
-        label="Default"
-        bg="active"
-        sx={{
-          marginLeft: '25px',
-          alignSelf: 'center',
-          alignItems: 'center',
-          padding: '3px 5px 2px 5px',
-        }}
+        bg="white"
+        sx={sx.expirationBadge}
+        textColor="text.primary"
+        label={expirationText}
       />
-    );
-  };
+    </Box>
+  );
 
-  const ExprirationBadge = props => {
-    const {
-      expirationText,
-      badgeText,
-    } = props;
 
-    return (
-      <Box
-        sx={!badgeText && {
-          paddingLeft: '72px',
-        }}
-        flexGrow={1}
-        alignItems="center"
-        alignSelf="center"
-      >
-        <Badge
-          bg="white"
-          sx={{
-            border: 'solid 1px',
-            borderColor: 'neutral.80',
-            alignSelf: 'center',
-            alignItems: 'center',
-            padding: '3px 5px 2px 5px',
-          }}
-          textColor="text.primary"
-          label={expirationText}
-        />
+  const ListElementContent = ({
+    badgeText,
+    isChild,
+    name,
+    policyId,
+  }) => (
+    <Box
+      isRow
+      ml={isChild ? 0 : 18}
+      sx={sx.listElement.textBox}
+    >
+      {isChild && (
+      <Separator
+        orientation="vertical"
+        sx={sx.bottomBracket.separator}
+      />
+      )}
+      <Box>
+        <Text
+          sx={sx.listElement.text}
+        >
+          {name}
+        </Text>
+        <Text
+          sx={sx.listElement.subText}
+        >
+          Policy Id:
+          {' '}
+          {policyId}
+        </Text>
       </Box>
-    );
-  };
+      {badgeText && <DefaultBadge />}
+    </Box>
+  );
 
-  // jsx of the production row
-  const Production = props => {
+  const ListElement = ({
+    badgeText,
+    expirationText,
+    isChild,
+    isParent,
+    name,
+    policyId,
+  }) => {
+    const listItemRef = useRef();
+
     const {
-      badgeText,
-      hasStaging,
-      policyId,
-      name,
-    } = props;
+      handleHoverEnd,
+      handleHoverStart,
+      handleMenuHoverEnd,
+      handleMouseMove,
+      isHovered,
+    } = useOverlappingMenuHoverState({ listItemRef });
 
     return (
-      <Box isRow height="75px">
-        <Box alignItems="center" justifyContent="center" sx={{ position: 'relative' }}>
-          <Icon
-            icon={ShieldVector}
-            color={hasStaging ? 'decorative.7' : 'accent.40'}
-            size={24}
-            isLinked={hasStaging}
-          />
-          {hasStaging && <TopBracket />}
-        </Box>
-        <Box isRow mr="auto" alignSelf="center" justifyContent="center" ml="18px" alignItems="center">
-          <Box>
-            <Text
-              sx={{
-                fontWeight: 1,
-                color: 'text.primary',
-                fontSize: 'md',
-              }}
-            >
-              {name}
-            </Text>
-            <Text
-              sx={{
-                fontWeight: 0,
-                color: 'text.secondary',
-                fontSize: 'sm',
-              }}
-            >
-              Policy Id:
-              {' '}
-              {policyId}
-            </Text>
-          </Box>
-          {
-            badgeText
-            && <DefaultBadge />
-          }
-        </Box>
-        <IconWithPopover />
-      </Box>
-    );
-  };
-
-  // jsx of the staging row.
-  const Staging = props => {
-    const {
-      badgeText,
-      expirationText,
-      policyId,
-      name,
-    } = props;
-
-    return (
-      <Box
+      <ListItem
+        isHovered={isHovered}
         isRow
-        height="75px"
-        marginLeft="12px"
+        onHoverEnd={handleHoverEnd}
+        onHoverStart={handleHoverStart}
+        onMouseMove={handleMouseMove}
+        ref={listItemRef}
+        variant="listItem.hover"
+        sx={sx.listElement.container}
       >
-        <BottomBracket />
-        <Box isRow alignItems="center" justifyContent="center">
-          <Box pl="0px" height="30px" flexDirection="row" alignItems="center">
-            <Separator
-              orientation="vertical"
-              sx={{
-                width: '2px !important',
-                backgroundColor: 'decorative.7',
-                marginLeft: '2px !important',
-              }}
-            />
-          </Box>
-          <Box>
-            <Text
-              sx={{
-                fontWeight: 1,
-                color: 'text.primary',
-                fontSize: 'md',
-              }}
-            >
-              {name}
-            </Text>
-            <Text
-              sx={{
-                fontWeight: 0,
-                color: 'text.secondary',
-                fontSize: 'sm',
-              }}
-            >
-              Policy Id:
-              {' '}
-              {policyId}
-            </Text>
-          </Box>
-        </Box>
-        {
-          badgeText
-          && <DefaultBadge />
-        }
-        {
-          expirationText
-          && <ExprirationBadge expirationText={expirationText} />
-        }
-        <IconWithPopover />
-      </Box>
+        {!isChild ? <Shield isParent={isParent} /> : <BottomBracket />}
+        <ListElementContent
+          badgeText={badgeText}
+          expirationText={expirationText}
+          isChild={isChild}
+          name={name}
+          policyId={policyId}
+        />
+        {expirationText && <ExpirationBadge expirationText={expirationText} />}
+        <IconWithPopover
+          handleHoverEnd={handleHoverEnd}
+          handleMenuHoverEnd={handleMenuHoverEnd}
+          handleHoverStart={handleHoverStart}
+        />
+      </ListItem>
     );
   };
 
   return (
-    <OverlayProvider>
-      <ListView {...args} items={theseItems}>
-        {item => (
-          <Item
+    <ListView {...args} items={theseItems}>
+      {item => (
+        <Item
+          {...item}
+          textValue={item.name}
+          data-id={item.key}
+          listItemProps={{ sx: sx.listViewItem }}
+        >
+          <ListElement
+            isParent={item.childrenObjects}
+            isChild={item.parentId}
             {...item}
-            textValue={item.name}
-            data-id={item.key}
-            listItemProps={{
-              minHeight: '75px',
-              sx: {
-                '&.has-inset-separator': {
-                  '&:before': {
-                    content: '""',
-                    position: 'absolute',
-                    width: 'calc(100% - 43px)',
-                    right: 0,
-                    bottom: 0,
-                    borderBottom: '1px solid',
-                    borderBottomColor: 'line.light',
-                  },
-                },
-              },
-            }}
-          >
-            {
-              item.parentId
-                ? <Staging {...item} hasInsetSeparator />
-                : <Production hasStaging={item.childrenObjects} {...item} />
-            }
-          </Item>
-        )}
-      </ListView>
-    </OverlayProvider>
+          />
+        </Item>
+      )}
+    </ListView>
   );
 };
