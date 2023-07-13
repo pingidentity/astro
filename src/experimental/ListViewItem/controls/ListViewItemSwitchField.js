abf353055f09fa3508383ec0b91eee1e9a9c629b
Jeremy Bissonnette <jeremybissonnette@pingidentity.com>
2023-07-13 19:04:14 +0000

    feat: [UIP-6353]: Add ListViewItem
    ## Describe your changes
    Due to the [original MR](https://gitlab.corp.pingidentity.com/ux/pingux/-/merge_requests/2435?diff_id=674653#163ab50ccc8fa60d6af637316db29b5db6a435a1) being merged to develop prematurely, a revert commit had to be made and merged to develop. This deleted the StyledListItem branch, and marked it the MR as merged. A new MR needed to be made so we can now merge the ListViewItem feature to develop.
    
    ## Checklist before requesting a review
    - [:white_check_mark: ] I have performed a self-review of my code
    - [:white_check_mark: ] I have added tests for any new or fixed functionality to prevent regressions
    - [:white_check_mark: ] I have added clarifying code comments to blocks which other devs may have questions on
    
    Closes UIP-6353

---
+++ src/experimental/ListViewItem/controls/ListViewItemSwitchField.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { switchFieldPropTypes } from '../../../components/SwitchField/switchFieldAttributes';
+import { SwitchField } from '../../../index';
+
+/**
+ * ListViewItemSwitchField is one of several styled control components that can be used as a child
+ * nested inside of the [ListViewItem](./?path=/story/experimental-listviewitem--default)..
+ * Its a wrapper component around the [SwitchField](./?path=/story/form-switchfield--default),
+ * intended to make matching specs easy. Most props available to the SwitchField are also
+ * available to ListItemEditButton.
+*/
+
+const ListViewItemSwitchField = props => {
+  return (
+    <SwitchField
+      aria-label="active user"
+      isDefaultSelected
+      mr={0}
+      {...props}
+    />
+  );
+};
+
+ListViewItemSwitchField.displayName = 'ListViewItemSwitchField';
+
+ListViewItemSwitchField.propTypes = switchFieldPropTypes;
+
+export default ListViewItemSwitchField;
