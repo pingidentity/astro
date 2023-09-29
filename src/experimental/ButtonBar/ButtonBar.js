ab7d28e25f9090677b5bcd871532f947c2adb279
James Robertson <jamesrobertson@pingidentity.com>
2023-09-29 11:27:30 +0000

    fix: [UIP-6449] ButtonBar refactor to return children only
    ## Describe your changes
    Refactored ButtonBar to return children only. This MR also address UIP-6450 to change isJustifiedRight prop to be align instead. 
    
    ## Any testing tips
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [x] I have added tests for any new or fixed functionality to prevent regressions
    - [ ] I have added clarifying code comments to blocks which other devs may have questions on
    
    Closes UIP-6449

--- src/experimental/ButtonBar/ButtonBar.js
+++ src/experimental/ButtonBar/ButtonBar.js
@@ -1,87 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { buttonPropTypes } from '../../components/Button/buttonAttributes';
-import { Box, Button } from '../../index';
-
-const SaveButton = props => (
-  <Button variant="primary" data-id="save-button" {...props}>
-    {props.text}
-  </Button>
-);
-
-const CancelButton = props => (
-  <Button variant="link" data-id="cancel-button" {...props}>
-    {props.text}
-  </Button>
-);
-
-const RefreshButton = props => (
-  <Button data-id="refresh-button" {...props}>
-    {props.text}
-  </Button>
-);
+import { Box } from '../../index';
 
 const ButtonBar = props => {
   const {
-    saveButtonProps,
-    cancelButtonProps,
-    refreshButtonProps,
-    isJustifiedRight,
+    align = 'left',
     children,
     ...others
   } = props;
-
-  const ButtonArray = [
-    <SaveButton {...saveButtonProps} />,
-    <CancelButton {...cancelButtonProps} />,
-  ];
-
-  const Content = () => {
-    const content = isJustifiedRight ? ButtonArray.slice().reverse() : ButtonArray;
-
-    if (refreshButtonProps) {
-      content.splice(1, 0, <RefreshButton {...refreshButtonProps} />);
-    }
-
-    return (
-      <>
-        {content.map(child => {
-          return child;
-        })}
-      </>
-    );
-  };
 
   return (
     <Box
       isRow
       gap="md"
-      sx={{ bg: 'white', px: 'lg', py: 'md', justifyContent: isJustifiedRight ? 'right' : 'left' }}
+      sx={{ bg: 'white', px: 'lg', py: 'md', justifyContent: align === 'right' ? 'right' : 'left' }}
       {...others}
     >
-      { children ? [...children] : <Content />}
+      {children}
     </Box>
   );
 };
 
 ButtonBar.propTypes = {
-  saveButtonProps: PropTypes.shape(buttonPropTypes),
-  cancelButtonProps: PropTypes.shape(buttonPropTypes),
-  refreshButtonProps: PropTypes.shape(buttonPropTypes),
-  isJustifiedRight: PropTypes.bool,
-};
-
-SaveButton.propTypes = {
-  text: PropTypes.string,
-};
-
-CancelButton.propTypes = {
-  text: PropTypes.string,
-};
-
-RefreshButton.propTypes = {
-  text: PropTypes.string,
+  align: PropTypes.oneOf(['left', 'right']),
 };
 
 export default ButtonBar;
--- src/experimental/ButtonBar/ButtonBar.test.js
+++ src/experimental/ButtonBar/ButtonBar.test.js
@@ -1,27 +1,38 @@
 import React from 'react';
 
-import { ButtonBar } from '../..';
+import { Button, ButtonBar } from '../..';
 import axeTest from '../../utils/testUtils/testAxe';
 import { render, screen } from '../../utils/testUtils/testWrapper';
 
 const testId = 'test-ButtonBar';
 
-const saveButtonProps = { key: 'save button', text: 'Save', onPress: () => alert('Save button pressed') };
-const cancelButtonProps = { key: 'cancel button', text: 'Cancel', onPress: () => alert('Cancel button pressed') };
-const refreshButtonProps = { key: 'refresh button', text: 'Refresh', onPress: () => alert('Refresh button pressed') };
+const defaultProps = { 'data-testid': testId };
 
-const defaultProps = { 'data-testid': testId, saveButtonProps, cancelButtonProps };
+const getComponent = (props = {}) => render(
+  <ButtonBar {...defaultProps} {...props}>
+    <Button
+      variant="primary"
+      data-id="save-button"
+      onPress={() => alert('Save button pressed')}
+    >
+      Save
+    </Button>
+    <Button
+      variant="link"
+      data-id="cancel-button"
+      onPress={() => alert('Cancel button pressed')}
+    >
+      Cancel
+    </Button>
+  </ButtonBar>,
+);
 
-const getComponent = (props = {}) => render((
-  <ButtonBar {...defaultProps} {...props} />
-));
-
-const getComponentCustomChildren = (props = {}) => render((
+const getComponentCustomChildren = (props = {}) => render(
   <ButtonBar {...defaultProps} {...props}>
     <button>custom text</button>
     <button>Also custom text</button>
-  </ButtonBar>
-));
+  </ButtonBar>,
+);
 
 const getComponentTextChildren = (props = {}) => render((
   <ButtonBar {...props}>
@@ -41,26 +52,13 @@ test('ButtonBar does render', () => {
   expect(element).toBeInTheDocument();
 });
 
-test('renders save and cancel buttons', () => {
+test('renders child save and cancel buttons', () => {
   getComponent();
 
-  const saveButton = screen.getByText(saveButtonProps.text);
+  const saveButton = screen.getByText('Save');
   expect(saveButton).toBeInTheDocument();
 
-  const cancelButton = screen.getByText(cancelButtonProps.text);
-  expect(cancelButton).toBeInTheDocument();
-});
-
-test('renders all three buttons', () => {
-  getComponent({ refreshButtonProps });
-
-  const saveButton = screen.getByText(saveButtonProps.text);
-  expect(saveButton).toBeInTheDocument();
-
-  const refreshButton = screen.getByText(refreshButtonProps.text);
-  expect(refreshButton).toBeInTheDocument();
-
-  const cancelButton = screen.getByText(cancelButtonProps.text);
+  const cancelButton = screen.getByText('Cancel');
   expect(cancelButton).toBeInTheDocument();
 });
 
@@ -80,8 +78,27 @@ test('renders custom text', () => {
   expect(firstText).toBeInTheDocument();
 });
 
-test('isJustifiedRight reverses order of button', () => {
-  getComponent({ isJustifiedRight: true });
-  const buttons = screen.getAllByRole('button');
-  expect(buttons[0]).toHaveAttribute('data-id', 'cancel-button');
+test('justify-content right when align prop set to right', () => {
+  getComponent({ align: 'right' });
+  const element = screen.getByTestId(testId);
+
+  expect(element).toHaveStyleRule('justify-content', 'right');
+});
+
+test('justify-content left when align prop is set to left', () => {
+  getComponent({ align: 'left' });
+  const element = screen.getByTestId(testId);
+
+  expect(element).toHaveStyleRule('justify-content', 'left');
+});
+
+test('justify-content left when align prop is excluded', () => {
+  getComponent();
+  const element = screen.getByTestId(testId);
+
+  expect(element).toHaveStyleRule('justify-content', 'left');
+});
+
+test('an error is thrown when align has invalid prop value', () => {
+  expect(() => getComponent({ align: 'rihgt' })).toThrow('Failed prop type');
 });
