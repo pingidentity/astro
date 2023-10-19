5988251593cc57382a801b84c83f2ce48719a0ac
Volodymyr Svirhun <volodymyrsvirhun@pingidentity.com>
2023-10-19 05:57:05 +0000

    refactor: [UIP-6549] Trial Experience Buttons: change story to match new designs and add figma link
    ## Describe your changes
    - add Figma link
    - Updated the story, removed a few buttons, changed the width of some divs.
    
    ## Any testing tips
    Verify that story has figma ling and match the design https://www.figma.com/file/bpbEw54gTTcdIiZs4PKK1p/Astro-Specs?node-id=43369%3A445&mode=dev
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [n/a] I have added tests for any new or fixed functionality to prevent regressions
    - [n/a] I have added clarifying code comments to blocks which other devs may have questions on

--- src/utils/designUtils/figmaLinks.js
+++ src/utils/designUtils/figmaLinks.js
@@ -110,5 +110,7 @@ export const FIGMA_LINKS = {
   slider: {
     default: 'https://www.figma.com/file/bpbEw54gTTcdIiZs4PKK1p/Astro-Specs?node-id=43112%3A1773&mode=dev',
   },
-
+  trialExperienceButtons: {
+    default: 'https://www.figma.com/file/bpbEw54gTTcdIiZs4PKK1p/Astro-Specs?node-id=43369%3A445',
+  },
 };
