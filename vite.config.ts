0c25e63fbbf3acbfd893a41f76d25d2957cf9eeb
Ridhisha Dangol <ridhishadangol@pingidentity.com>
2025-10-21 09:00:42 -0600

    fix: output path for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: './dist/astro',
+    outDir: '../../dist/astro',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
