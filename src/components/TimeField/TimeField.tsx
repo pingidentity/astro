e6d7cccd8bcc4456e7a66d1f99ffd422d8b56b5f
Ridhisha Dangol <ridhishadangol@pingidentity.com>
2026-01-08 16:17:35 -0700

    ci: remove log files and clean up

--- src/components/TimeField/TimeField.tsx
+++ src/components/TimeField/TimeField.tsx
@@ -24,6 +24,8 @@ const TimeField = forwardRef<HTMLDivElement, TimeFieldProps>((props, ref) => {
   const timeFieldRef = useLocalOrForwardRef<HTMLDivElement>(ref);
 
   const fieldRef = useRef<HTMLElement>(null);
+  console.log('🚀 ~ fieldRef:', fieldRef);
+  const test = 'hello';
   const labelRef = useRef<HTMLLabelElement>(null);
 
   const parsedTimes = {
