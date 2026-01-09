0dc87f35bbf7783471ff7dace919aa426bad546a
akeohanePing <andrewkeohane@pingidentity.com>
2026-01-09 15:18:41 -0500

    fix: [UIP-8039] fix timezones file extension

--- src/components/TimeZonePicker/TimeZonePicker.jsx
+++ src/components/TimeZonePicker/TimeZonePicker.jsx
@@ -12,7 +12,7 @@ import PropTypes from 'prop-types';
 import { Box, ComboBoxField, Item, Text } from '../../index';
 import { getPendoID } from '../../utils/devUtils/constants/pendoID';
 
-import defaultTimezones, { usCities } from './timezones.jsx';
+import defaultTimezones, { usCities } from './timezones';
 
 const createSearchTags = ({ gmt, gmtLabel, timeZone }) => {
   let additionalTags = '';
